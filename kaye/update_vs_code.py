767b735c33c08bfa84e05fe46c80c14e1938d0be
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-12-12 05:18:27 -0800

    add settings.ini

--- kaye/update_vs_code.py
+++ kaye/update_vs_code.py
@@ -1,6 +1,28 @@
+"""TODO
+"""
+
+PROGRAM_NAME = 'kaye.update_vs_code'
+
+
+from argparse import ArgumentParser, RawTextHelpFormatter
 
 
 from get_prompt import get_prompt
 
 
 # TODO update vscode prompts & update model
+
+
+psr = ArgumentParser(prog=PROGRAM_NAME,
+        description=__doc__, formatter_class=RawTextHelpFormatter)
+
+# positional arguments
+psr.add_argument('PROMPT',
+        help='PROMPT name')
+
+
+if __name__ == "__main__":
+    args = psr.parse_args()
+
+
+    exit(0)
