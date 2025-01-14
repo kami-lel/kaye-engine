3aac2bebbfa155e884349a4c8a5c6d2fd9c0ff8e
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-01-14 10:49:25 -0800

    improve update_vsc module help docs

--- kaye/update_vsc.py
+++ kaye/update_vsc.py
@@ -1,11 +1,13 @@
-"""modify settings.json file of VS code
-"""
+ENV_VAR_TOKEN = "VSC_SETTING_JSON_PATH"
+
+__doc__ = """modify settings.json file of VS code, 
+specified by an enviornment variable {}
+""".format(ENV_VAR_TOKEN)
 
 
-PROGRAM_NAME = 'kaye.update_vsc'
-SYSTEM_MESSAGE_KEY = 'genieai.systemMessage'  # in settings.json
-COMMIT_MESSAGE_KEY = 'genieai.promptPrefix.commit-message'
-ENV_VAR_TOKEN = 'KAYE_UPDATE_VSC_SETTING_JSON_PATH'
+PROGRAM_NAME = "kaye.update_vsc"
+SYSTEM_MESSAGE_KEY = "genieai.systemMessage"  # in settings.json
+COMMIT_MESSAGE_KEY = "genieai.promptPrefix.commit-message"
 
 
 from argparse import ArgumentParser, RawTextHelpFormatter, FileType
@@ -17,31 +19,36 @@ import json
 from kaye.get_prompt import get_prompt, PROMPTS, PROMPT_DOC
 
 
-
-psr = ArgumentParser(prog=PROGRAM_NAME,
-        description=__doc__ + PROMPT_DOC,
-        formatter_class=RawTextHelpFormatter)
+psr = ArgumentParser(
+    prog=PROGRAM_NAME,
+    description=__doc__ + PROMPT_DOC,
+    formatter_class=RawTextHelpFormatter,
+)
 
 
 # positional arguments
-psr.add_argument('PROMPT',
-        choices=list(PROMPTS.keys()),
-        help='name of a predefined prompt')
+psr.add_argument(
+    "PROMPT", choices=list(PROMPTS.keys()), help="name of a predefined prompt"
+)
 
 
 # options
-psr.add_argument('-f', '--file',
-        action='store',
-        type=FileType('r+'),
-        required=False,
-        metavar='SETTING_JSON',
-        help='update settings.json at SETTING_JSON, instead default location')
-psr.add_argument('-c', '--change-commit-message',
-        action='store_true',
-        help='update Commit Message')
-psr.add_argument('-v', '--verbose',
-        action='store_true',
-        required=False)
+psr.add_argument(
+    "-f",
+    "--file",
+    action="store",
+    type=FileType("r+"),
+    required=False,
+    metavar="SETTING_JSON",
+    help="update settings.json at SETTING_JSON, instead default location",
+)
+psr.add_argument(
+    "-c",
+    "--change-commit-message",
+    action="store_true",
+    help="update Commit Message",
+)
+psr.add_argument("-v", "--verbose", action="store_true", required=False)
 
 
 if __name__ == "__main__":
@@ -51,8 +58,10 @@ if __name__ == "__main__":
     try:
         content = get_prompt(args.PROMPT)
     except ValueError as err:
-        print('Error: {} of arg PROMPT not recognized'
-                .format(args.PROMPT), file=stderr)
+        print(
+            "Error: {} of arg PROMPT not recognized".format(args.PROMPT),
+            file=stderr,
+        )
         exit(errno.EINVAL)
 
     # decide where settings.json is
@@ -61,31 +70,39 @@ if __name__ == "__main__":
     elif getenv(ENV_VAR_TOKEN):
         dest = getenv(ENV_VAR_TOKEN)
     else:
-        print("Error: enviornment variable {} not set"
-                .format(ENV_VAR_TOKEN), file=stderr)
+        print(
+            "Error: enviornment variable {} not set".format(ENV_VAR_TOKEN),
+            file=stderr,
+        )
         exit(errno.ENONET)
 
     # update the file
     try:
-        with open(dest, 'r', encoding='utf-8', newline='') as file:
+        with open(dest, "r", encoding="utf-8", newline="") as file:
             data = json.load(file)
 
     except json.JSONDecodeError as err:
-        print('Error: JSON: {}'.format(err.args[0]), file=stderr)
+        print("Error: JSON: {}".format(err.args[0]), file=stderr)
         exit(errno.EINVAL)
     except (FileNotFoundError, PermissionError) as err:
-        print('Error: {}: {}'.format(err.args[1], dest))
+        print("Error: {}: {}".format(err.args[1], dest))
         exit(err.errno)
 
-    key = COMMIT_MESSAGE_KEY if args.change_commit_message \
-            else SYSTEM_MESSAGE_KEY
+    key = (
+        COMMIT_MESSAGE_KEY
+        if args.change_commit_message
+        else SYSTEM_MESSAGE_KEY
+    )
     data[key] = content
 
-    with open(dest, 'w', encoding='utf-8', newline='') as file:
+    with open(dest, "w", encoding="utf-8", newline="") as file:
         json.dump(data, file, indent=4)
 
     if args.verbose:
-        print('prompt {} saved into VS code settins: {}'.format(
-                args.PROMPT, dest))
+        print(
+            "prompt {} saved into VS code settins: {}".format(
+                args.PROMPT, dest
+            )
+        )
 
     exit(0)
