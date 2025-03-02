927216abd9627e1afa19f393f75c24bc3c9bc449
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-03-01 16:38:25 -0800

    add string representation methods to PromptParserNode class

--- kaye/gen_prompt/prompt_parser_node.py
+++ kaye/gen_prompt/prompt_parser_node.py
@@ -7,6 +7,12 @@ HEADING_MARKER = "#"
 
 class PromptParserNode(OrderedDict):
 
+    def __str__(self):
+        return ""  # TODO
+
+    def __repr__(self):
+        return ""  # TODO
+
     def __new__(cls, level, parent, full_prompt):
         return super().__new__(cls, {})  # new as empty dict
 
@@ -22,32 +28,23 @@ class PromptParserNode(OrderedDict):
     @staticmethod
     def _convert_full_prompt2text_list(full_prompt):
         """
-        Convert the given full prompt string into a list of text lines.
-
-        This method takes a full prompt string, cleans up any consecutive empty
-        lines, and splits the string into a list of individual lines.
-
         :param full_prompt: The full prompt string to be converted.
         :type full_prompt: str
         :return: A list of lines from the cleaned-up prompt string.
-        :rtype: list of str
+        :rtype: list(str)
         """
-        # clean up consecutive empty lines
-        # FIXME clean up pre- / post- heading empty line
+        # remove all empty lines
         cleanup = re.sub(rf"{LF}+", LF, full_prompt)
         return list(cleanup.split(LF))
 
     def _init_populate_by_text_list(self, lines):
         heading_prefix = HEADING_MARKER * (self.level + 1) + " "
 
-        # find every sub-section heading line
-        heading_lines = OrderedDict()
+        # find every sub-section heading lines
+        heading_lines = []
         for idx, line in enumerate(lines):
             if line.startswith(heading_prefix):
-                # extract heading content
-                # e.g. "### this is heading " -> "this is heading"
-                heading_content = line[len(heading_prefix) :].strip()
-                heading_lines[idx] = heading_content
+                heading_lines.append(idx)
 
-        # e.g. now, heading_lines = {0: 'personality',
-        #       38: 'conversation', 95: 'abbreviation', 512: 'role'}
+        heading_lines.insert(0, 0)
+        heading_lines.append(len(lines))
