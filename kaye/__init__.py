b543e7382382314f3caf046008c18175d8b4bc7d
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-03-04 14:29:24 -0800

    add PromptTreeNode class with unit tests for parsing functionality

---
+++ kaye/__init__.py
@@ -0,0 +1,1 @@
+from .prompt_tree_node import *
---
+++ kaye/prompt_tree_node.py
@@ -0,0 +1,75 @@
+# TODO module doc
+
+import re
+from collections import OrderedDict
+
+HEADING_MARKER = "#"
+
+
+class PromptTreeNode(OrderedDict):
+    """
+    represents a single node in a Prompt Tree structure
+
+    :param text:
+    :type text: str or list(str)
+    :param level: level which this node exist, e.g.
+
+    - ``0`` for root node (entire document)
+    - ``1`` for 1st level section, i.e. an section of ``# heading``
+    - etc.
+
+    :type level: int, optional
+    :param parent: parent of the node in the tree; ``None`` if root node
+    :type parent: PromptTreeNode
+    """
+
+    def __repr__(self, indent=0, column_width_limit=79):
+        return ""
+
+    def __new__(cls, text, parent=None):
+        return super().__new__(cls, {})  # new as empty dict
+
+    def __init__(self, text, parent=None):
+        self.parent = parent
+        self.content = ""
+
+        if parent is None:  # when current node is root
+            self.level = 0
+            text = self._convert_full_prompt2str_list_per_line(text)
+        else:
+            self.level = parent.level + 1
+
+        self._populate_self_by_str_line(text)
+
+    @staticmethod
+    def _convert_full_prompt2str_list_per_line(full_prompt):
+        cleanup = re.sub(rf"\n+", "\n", full_prompt)
+        # remove all empty lines
+        return list(cleanup.split("\n"))
+
+    def _populate_self_by_str_line(self, lines):
+        heading_prefix = HEADING_MARKER * (self.level + 1) + " "
+
+        # find every sub-section heading lines
+        heading_lines = []
+        for idx, line in enumerate(lines):
+            if line.startswith(heading_prefix):
+                heading_lines.append(idx)
+
+        if not heading_lines:  # contain no subsection
+            self.content = "\n".join(lines)  # all lines are content
+            return
+
+        # this node contains subsections
+        # parse the content part out
+        self.content = "\n".join(lines[: heading_lines[0]])
+
+        # parse sub-sections as nodes
+        heading_lines.append(len(lines))
+        for start, end in zip(heading_lines, heading_lines[1:]):
+            # extract heading content
+            # e.g. "### this is heading " -> "this is heading"
+            heading_content = lines[start][len(heading_prefix) :].strip()
+            self[heading_content] = PromptTreeNode(
+                lines[start + 1 : end], self
+            )
