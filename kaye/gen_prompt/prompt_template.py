30ac4d25d1e35a80ac7ec1b68fa552f5d958a6f7
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-03-16 22:58:08 -0700

    update PromptTemplate class for better configurability and clarity
    prompt_template.py: refactor constructor for improved parameter clarity and add detailed docstring for string representation method

--- kaye/gen_prompt/prompt_template.py
+++ kaye/gen_prompt/prompt_template.py
@@ -16,23 +16,60 @@ NO_CHECKBOX = "   "
 
 class PromptTemplate:
 
-    def __init__(self, tree_root=None, saved_prompt_template=None):
-        self.enables = [TREE_ROOT_NAME]
-        self.tree_root = tree_root or get_current_full_prompt_tree()
+    def __init__(
+        self,
+        savable_prompt_template=None,
+        detached_mode=False,
+        full_prompt_tree=None,
+    ):
+        self.enabled_nodes_names = []
+        # enable tree root node means non-detached mode
+        if not detached_mode:
+            self.enabled_nodes_names.append(TREE_ROOT_NAME)
+
+        self.full_prompt_tree = (
+            full_prompt_tree or get_current_full_prompt_tree()
+        )
+
+        if savable_prompt_template:
+            self._init_populate_enabled_nodes_names(savable_prompt_template)
+
+    def _init_populate_enabled_nodes_names(self, savable_prompt_template):
+        pass  # TODO
 
     def __repr__(self, preview_line_count=3, preview_line_width=64):
-        # TODO docstring
+        """
+        Returns a string representation of the PromptTemplate and its
+        enabled nodes in a human-readable format, allowing a preview of
+        each node's content.
 
+        The representation includes checkbox indicators for whether each
+        node is enabled (checked or unchecked) along with the respective
+        lines of content previewed according to the specified parameters.
+
+        :param preview_line_count: The number of lines to preview for the
+                content of the node; defaults to 3.
+        :type preview_line_count: int
+        :param preview_line_width: The width of each preview line,
+               which determines how many characters from the content will be
+               included in the preview; defaults to 64.
+        :type preview_line_width: int
+        :return: A string representation of the current node, with enabled
+               nodes and their content previews included.
+        :rtype: str
+        """
         opt_lines = []
 
-        for pre, fill, node in RenderTree(self.tree_root):
+        for pre, fill, node in RenderTree(self.full_prompt_tree):
             node_name = node.name
             # decide either have [x] or [ ] before node lines
             checkbox_prefix = (
-                CHECKED_BOX if node_name in self.enables else UNCHECKED_BOX
+                CHECKED_BOX
+                if node_name in self.enabled_nodes_names
+                else UNCHECKED_BOX
             )
 
-            opt_lines.append(checkbox_prefix + pre + node.name)
+            opt_lines.append(checkbox_prefix + pre + node_name)
 
             # lines for the content of node
             opt_lines.extend(
