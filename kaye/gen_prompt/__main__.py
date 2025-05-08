69b579294306231f1e0a582454c7f22c23b45782
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-05-08 04:05:30 -0700

    add argument parsing scaffold and placeholders to main module
    __main__.py: add ArgumentParser setup, main guard, and TODO placeholders for features

--- kaye/gen_prompt/__main__.py
+++ kaye/gen_prompt/__main__.py
@@ -1,1 +1,29 @@
-# TODO write use argparser
+"""test"""
+
+# Todo docstring
+
+import sys
+
+
+from argparse import ArgumentParser
+
+PROGRAM_NAME = "kaye.gen_prompt"
+
+psr = ArgumentParser(prog=PROGRAM_NAME, description=__doc__)
+
+ls_psr = psr.add_subparsers(dest="ls")
+
+
+# positional argument
+# options
+
+
+if __name__ == "__main__":
+    args = psr.parse_args()
+
+    # TODO ls prompts
+    # TODO prompt name => prompt content
+    # TODO interactive mode
+    # TODO corpus from
+
+    sys.exit()
