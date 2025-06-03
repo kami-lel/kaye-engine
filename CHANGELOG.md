0eaf6902c3b4fd0a6b1a50ce4cd3ed4ae236afe3
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-06-03 13:15:58 -0700

    add render datetime to blueprint version for alpha releases
    CHANGELOG.md: update changelog with new blueprint version feature
    prompt_blueprint.py: append render datetime to version for alpha releases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,4 @@
 v4.3.2-alpha
 
-re prompt_corpus.md
-
-- improve section: data declarations in 2d form
+- re prompt_corpus.md: improve section data declarations in 2d form
+- re prompt_blueprint.py: add feature of including render datetime as part of version
--- kaye/gen_prompt/prompt_blueprint.py
+++ kaye/gen_prompt/prompt_blueprint.py
@@ -3,6 +3,8 @@ define `PromptBlueprint`
 """
 
 import re
+from datetime import datetime
+
 import importlib.metadata
 from anytree import RenderTree, PreOrderIter
 
@@ -279,6 +281,11 @@ class PromptBlueprint:
         # create comment part
         if not hide_comment:
             kaye_version = importlib.metadata.version("kaye")
+
+            # append render date-time in version for alpha releases
+            if "a" in kaye_version:
+                kaye_version += datetime.now().strftime(".0%Y%m%d%H%M%S")
+
             comment_line = "<!-- {}Kaye v{} -->".format(
                 (
                     "blueprint:{}; ".format(self.blueprint_name)
