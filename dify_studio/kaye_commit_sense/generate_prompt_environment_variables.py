9f96eea708bc96b02c56155ba5eb6ef14e80e277
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-09-29 16:56:48 -0700

    add placeholder for prompt env variables generator
    [^]generate_prompt_environment_variables.py

---
+++ dify_studio/kaye_commit_sense/generate_prompt_environment_variables.py
@@ -0,0 +1,10 @@
+"""
+generate Environment Variables prefixed with ``prompt_`` for dify app
+"""
+
+
+from kaye.gen_prompt import load_embedded_prompt_corpus
+
+
+def __name__ == "__main__":
+    pass  # TODO
