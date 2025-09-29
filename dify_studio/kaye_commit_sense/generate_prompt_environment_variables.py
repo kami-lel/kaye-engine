2061d85133728453a996396275eae3270ca64205
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-09-29 16:57:32 -0700

    fix main guard syntax to enable script execution
    [@]generate_prompt_environment_variables.py

--- dify_studio/kaye_commit_sense/generate_prompt_environment_variables.py
+++ dify_studio/kaye_commit_sense/generate_prompt_environment_variables.py
@@ -2,9 +2,7 @@
 generate Environment Variables prefixed with ``prompt_`` for dify app
 """
 
-
 from kaye.gen_prompt import load_embedded_prompt_corpus
 
-
-def __name__ == "__main__":
+if __name__ == "__main__":
     pass  # TODO
