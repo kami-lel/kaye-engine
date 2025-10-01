f54273ad0e5aa5f241495487f54df18980e819f1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-01 16:43:54 -0700

    update Inline Comment Tags: Fixme to FIXME, Hack to HACK, Todo to TODO
    [@]prompt_blueprint_loader.py: fixme comment updated to uppercase FIXME for blueprint file detection
    [@]generate_static_prompts.py: correct annotation marker from lowercase hack to uppercase HACK for consistency
    [@]update_prompt_blueprints_structure.py: Fix annotation markers from lowercase to uppercase for consistency

--- kaye/gen_prompt/prompt_blueprint_loader.py
+++ kaye/gen_prompt/prompt_blueprint_loader.py
@@ -2,7 +2,7 @@
 define ``get_prompt_templates_names``, ``load_prompt_template``
 """
 
-# Fixme detect blueprints files as .kaye_blueprint
+# FIXME detect blueprints files as .kaye_blueprint
 
 import os
 from pathlib import Path
--- scripts/generate_static_prompts.py
+++ scripts/generate_static_prompts.py
@@ -38,4 +38,4 @@ if __name__ == "__main__":
             f.write(blueprint.generate_prompt(hide_comment=True))
 
 
-# Hack deprecation?
+# HACK deprecation?
