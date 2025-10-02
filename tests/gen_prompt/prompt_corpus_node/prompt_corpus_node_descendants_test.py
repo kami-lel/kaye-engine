6747e66d3106aaf8307028034ff59e76c4d72f96
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-02 13:28:42 -0700

    add comprehensive tests for get_descendants_paths in prompt_corpus_node_descendants_test.py using PROMPT3 to validate descendant paths for various nodes
    [+]prompt_corpus_node_descendants_test.py

--- tests/gen_prompt/prompt_corpus_node/prompt_corpus_node_descendants_test.py
+++ tests/gen_prompt/prompt_corpus_node/prompt_corpus_node_descendants_test.py
@@ -2,13 +2,9 @@
 test .get_descendants_paths()
 """
 
-# TODO
-
-
 from kaye.gen_prompt import PromptCorpusNode
 from tests.gen_prompt.prompt_corpus_node.testees import (
     PROMPT1,
-    PROMPT2,
     PROMPT3,
 )
 
@@ -40,5 +36,86 @@ class TestParse1:  # test using PROMPT1
         assert descendants_paths == []
 
 
-# TODO prompt2
-# TODO prompt3
+class TestParse3:  # test using PROMPT3
+
+    def test_root(self):
+        node = PromptCorpusNode.parse(PROMPT3)
+
+        descendants_paths = node.get_descendants_paths()
+
+        print(descendants_paths)
+        assert isinstance(descendants_paths, list)
+        assert descendants_paths == [
+            ["Main Title"],
+            ["Main Title", "Introduction"],
+            ["Main Title", "Introduction", "Background"],
+            ["Main Title", "Introduction", "Background", "Importance"],
+            [
+                "Main Title",
+                "Introduction",
+                "Background",
+                "Importance",
+                "Objective",
+            ],
+            ["Main Title", "Methods"],
+            ["Main Title", "Methods", "Data Collection"],
+            ["Main Title", "Methods", "Data Collection", "Tools Used"],
+            [
+                "Main Title",
+                "Methods",
+                "Data Collection",
+                "Tools Used",
+                "Future Work",
+            ],
+            ["Main Title", "Conclusion"],
+        ]
+
+    def test_node1(self):
+        tree = PromptCorpusNode.parse(PROMPT3)
+        main_title = tree.children[0]
+        node = main_title.children[0]
+
+        descendants_paths = node.get_descendants_paths()
+
+        print(descendants_paths)
+        assert descendants_paths == [
+            ["Main Title", "Introduction", "Background"],
+            ["Main Title", "Introduction", "Background", "Importance"],
+            [
+                "Main Title",
+                "Introduction",
+                "Background",
+                "Importance",
+                "Objective",
+            ],
+        ]
+
+    def test_node2(self):
+        tree = PromptCorpusNode.parse(PROMPT3)
+        main_title = tree.children[0]
+        node = main_title.children[1]
+
+        descendants_paths = node.get_descendants_paths()
+
+        print(descendants_paths)
+        assert descendants_paths == [
+            ["Main Title", "Methods", "Data Collection"],
+            ["Main Title", "Methods", "Data Collection", "Tools Used"],
+            [
+                "Main Title",
+                "Methods",
+                "Data Collection",
+                "Tools Used",
+                "Future Work",
+            ],
+        ]
+
+    def test_leaf(self):
+        tree = PromptCorpusNode.parse(PROMPT3)
+        parent = tree.children[0].children[0].children[0].children[0]
+        node = parent.children[0]
+
+        descendants_paths = node.get_descendants_paths()
+
+        print(descendants_paths)
+        assert descendants_paths == []
