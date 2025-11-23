192f1b44e085b3c0f9f9b8886c509409a1d37d34
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-11-22 17:12:52 -0800

    add usc, engageSC event url format constants, generation logic, update date range tuple, bug/feature todo notes
    [~]gen_urls.py: Refactor URL constants for clarity and multi-line format. Add new event sources: USC Event Calendar, Engage SC. Modify _create_date_range to return inclusive day count. Add TODOs and FIXMEs for weekend handling, page filtering, and date filters in some URLs. Enhance main() to generate URLs with new sources and date ranges. Comments added for future improvements and known bugs
    [@]note: add TODO to test necessity of register in event filter

--- dify_studio/kaye_event_radar/nodes/gen_urls.py
+++ dify_studio/kaye_event_radar/nodes/gen_urls.py
@@ -1,12 +1,27 @@
 from datetime import timedelta, date as date_cls
 
-EVENTBRITE_URL = "https://www.eventbrite.com/d/ca--los-angeles/all-events/?page=1&start_date={}&end_date={}"
+EVENTBRITE_URL = (
+    "https://www.eventbrite.com/d/ca--los-angeles/all-events/"
+    "?page=1&start_date={}&end_date={}"
+)
 EVENTBRITE_DATE = "%Y-%m-%d"
+
 DOWNTOWN_LA_URL = "https://downtownla.com/calendar#{}-to-{}"
 DOWNTOWN_LA_DATE = "%m-%d-%y"
+
 LA_LIVE_URL = "https://www.lalive.com/events/filter/{}/{}/"
 LA_LIVE_DATE = "%Y-%m-%d"
+
 DISCOVER_LA_URL = "https://www.discoverlosangeles.com/events"
+
+USC_EVENT_CALENDAR_URL = (
+    "https://calendar.usc.edu/calendar/day/"
+    "{}?card_size=small&order=date&days={}&experience="
+)
+USC_EVENT_CALENDAR_DATE = "%Y/%m/%d"
+
+ENGAGE_SC_URL = "https://engage.usc.edu/events?from_date={}&to_date={}"
+ENGAGE_SC_DATE = "%d+%b+%Y"
 
 
 def _create_date_range():
@@ -15,16 +30,18 @@ def _create_date_range():
     spanning for at max 1 week
 
 
-    :return: start & end date
+    :return: start date, end date, days count (inclusive)
     :rtype: tuple(datetime.date, datetime.date)
     """
     start_date = date_cls.today()
 
+    # Fixme when weekend (Fri, Sat, Sun, find coming week)
+    # BUG inclusive days count
     # find coming Sunday
-    days_ahead = ((6 - start_date.weekday()) % 7) or 7
-    end_date = start_date + timedelta(days=days_ahead)
+    days_cnt = ((6 - start_date.weekday()) % 7) or 7
+    end_date = start_date + timedelta(days=days_cnt)
 
-    return start_date, end_date
+    return start_date, end_date, days_cnt
 
 
 def main():
@@ -34,8 +51,9 @@ def main():
 
     :return: ``opt`` is a ``list`` of urls (``str``) to be scraped
     """
+    # TODO take list of website names from user input
 
-    start_date, end_date = _create_date_range()
+    start_date, end_date, days_cnt = _create_date_range()
 
     # create urls  -------------------------------------------------------------
     opt = []
@@ -58,13 +76,31 @@ def main():
     )
 
     # Discover LA
+    # Fixme no date filter
     opt.append(DISCOVER_LA_URL)
 
     # LA Live.com
+    # Bug filter not applied
     opt.append(
         LA_LIVE_URL.format(
             start_date.strftime(LA_LIVE_DATE),
             end_date.strftime(LA_LIVE_DATE),
+        )
+    )
+
+    # USC Event Calendar
+    # Fixme only page 1
+    opt.append(
+        USC_EVENT_CALENDAR_URL.format(
+            start_date.strftime(USC_EVENT_CALENDAR_DATE), days_cnt
+        )
+    )
+
+    # engageSC
+    opt.append(
+        ENGAGE_SC_URL.format(
+            start_date.strftime(ENGAGE_SC_DATE),
+            end_date.strftime(ENGAGE_SC_DATE),
         )
     )
 
