70f24daf5708d100fa54e941200393698ecb5272
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-07 22:40:32 -0800

    add function for dify app

---
+++ kaye/api/dify_app.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+from kaye import PROGRAM_NAME
+
+dify_bp = Blueprint("dify-app", PROGRAM_NAME, url_prefix="/kaye/dify-app")
+
+
+@dify_bp.route("/kaye-peer-coder/pre-sense", methods=["GET"])
+def kaye_peer_coder_pre_sense():  # todo
+    return ""  # TODO
+
+
+@dify_bp.route("/kaye-peer-coder/task", methods=["GET"])
+def kaye_peer_coder_task():  # todo
+    return ""  # TODO
