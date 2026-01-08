c700bb1783c6410012d7ba1fcd09169ea4da705e
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-07 18:24:51 -0800

    add basic structure

---
+++ dify_studio/kaye_peer_coder/nodes/prompt_assembler.py
@@ -0,0 +1,4 @@
+def main(prompt_collections: dict, programming_languages: list[str]):
+    # TODO
+    opt = ""
+    return {"assembled_prompt": opt}
--- scripts/create_peer_coder_prompts_collection.py
+++ scripts/create_peer_coder_prompts_collection.py
@@ -4,3 +4,5 @@ create_peer_coder_prompts_collection.py
 create PROMPTS_COLLECTION Conversation Variable for Dify app Kaye_Peer_Coder
 dynamically, to utilize the newest version of prompt corpus
 """
+
+# TODO
