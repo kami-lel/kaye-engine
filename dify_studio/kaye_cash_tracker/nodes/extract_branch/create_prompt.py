b3414d6f92dba8460f90c4100e0868ad065696b5
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-08 06:09:51 -0800

    finish code for fetching

--- dify_studio/kaye_cash_tracker/nodes/extract_branch/create_prompt.py
+++ dify_studio/kaye_cash_tracker/nodes/extract_branch/create_prompt.py
@@ -5,10 +5,16 @@ cache the template locally
 then fill the template with runtime info to produce the concrete prompt
 """
 
+import requests
+
+# constants  ###################################################################
 OUTPUT_PROMPT_KEY = "concrete_prompt"
 OUTPUT_TEMPLATE_KEY = "updated_prompt_template_cache"
+KAYE_API_URL = "http://localhost:11255/kaye/dify-app/kaye-cash-tracker/"
+REQUEST_TIMEOUT = 10
 
 
+# entry point  #################################################################
 def main(
     prompt_template_cache: str,
     today: str,
@@ -19,7 +25,15 @@ def main(
 
     if not prompt_template_cache:  # when not locally cached
         # get prompt template by API  ------------------------------------------
-        pass  # TODO
+        try:
+            response = requests.get(KAYE_API_URL, timeout=REQUEST_TIMEOUT)
+            response.raise_for_status()
+            prompt_template_cache = response.text
+
+        except requests.RequestException as err:
+            raise ValueError(
+                "fail to GET prompt: {}".format(err.args[0])
+            ) from err
 
     # fill template w/ runtime info  -------------------------------------------
     prompt = prompt_template_cache.format(
