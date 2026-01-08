05bbabe3a3e93d062d7ff49b802e716d3adde6c8
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-08 06:24:18 -0800

    update host ip

--- kaye/api/__init__.py
+++ kaye/api/__init__.py
@@ -5,7 +5,7 @@ from kaye.api.prompt import prompt_bp
 from kaye.api.dify_app import dify_bp
 
 # constants  ###################################################################
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 11255
 
 
