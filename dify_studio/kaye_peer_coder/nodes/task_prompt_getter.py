6762b9639cb924274078b2dfa5a079da6b2966a8
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-07 20:54:56 -0800

    finish task getter

--- dify_studio/kaye_peer_coder/nodes/task_prompt_getter.py
+++ dify_studio/kaye_peer_coder/nodes/task_prompt_getter.py
@@ -1,12 +1,14 @@
-from enum import Flag, auto
+from enum import IntFlag, auto
 
 TASK_PROMPT_KEY = "task_prompt"
 
 
-class Language(Flag):
+class PL(IntFlag):
     """
     represent a single programming language
     """
+
+    NONE = 0
 
     # pylint: disable=invalid-name
 
@@ -24,9 +26,27 @@ class Language(Flag):
     py = auto()
 
 
-def main(languages: dict, task_prompt_flags: float, task_prompt_cache: str):
+def _calc_flags(languages):
+    # TODO docstring
+    flags = PL.NONE
+    for lang in languages:
+        flags |= PL[lang]
+    return flags
 
-    # TODO
-    task_prompt = ""
+
+def main(languages: dict, task_prompt_flags: float, task_prompt_cache: str):
+    flags = _calc_flags(languages)
+    cached_flags = PL(int(task_prompt_flags))
+
+    if flags == 0:  # no programming
+        task_prompt = ""  # TODO
+
+    elif flags not in cached_flags:
+        # contains additional required languages
+        task_prompt = ""  # TODO
+
+    else:
+        # sending the cached/same prompt
+        task_prompt = task_prompt_cache
 
     return {TASK_PROMPT_KEY: task_prompt}
