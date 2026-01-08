1e726fe572564151b89750e4d1739c343b10075a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-08 11:06:48 -0800

    attempt for code for task

--- kaye/api/dify_app/kaye_peer_coder.py
+++ kaye/api/dify_app/kaye_peer_coder.py
@@ -4,7 +4,7 @@ define API to specific work with Dify App: Kaye Peer Coder
 
 # pylint: disable=missing-function-docstring
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 from kaye import PROGRAM_NAME
 from kaye.gen_prompt import PromptBlueprint, load_embedded_prompt_corpus
@@ -53,4 +53,8 @@ def kaye_peer_coder_pre_sense():
 # /kaye/dify-app/kaye-peer-coder/task
 @kyc_bp.route("/task", methods=["GET"])
 def kaye_peer_coder_task():
-    return "not implemented yet"  # TODO
+    # BUG need test
+    languages_arg = request.args.get("languages")
+
+    result = {"abc": 1, "def": "ghi", "languages": languages_arg}
+    return jsonify(result)
