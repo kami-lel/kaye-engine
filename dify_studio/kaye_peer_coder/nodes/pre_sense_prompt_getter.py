fa23d134ea1759100cae4b1a21ba666407c2441e
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-07 19:59:00 -0800

    add requried nodes

---
+++ dify_studio/kaye_peer_coder/nodes/pre_sense_prompt_getter.py
@@ -0,0 +1,21 @@
+"""
+get `pre-sense` node's prompts from Kaye's Flask API
+"""
+
+KEY_IN_PROMPTS_CACHE = "pre-sense"
+
+
+def main(prompts_cache: dict):
+    # prompt already in cache
+    if KEY_IN_PROMPTS_CACHE in prompts_cache:
+        system_message = prompts_cache[KEY_IN_PROMPTS_CACHE]
+
+    else:
+        # get prompt from API  -------------------------------------------------
+        # Todo use API
+        system_message = ""
+
+        # update Conversation Variable
+        prompts_cache[KEY_IN_PROMPTS_CACHE] = system_message
+
+    return {"system_message": system_message, "prompts_cache": prompts_cache}
