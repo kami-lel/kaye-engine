99103d7d5aea08bd6612588ffb1cde31fe18d217
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-09 08:57:47 -0800

    update docstring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,9 @@
 <!-- fixme merge role: Grammar Checker & Etiquette Coach -->
 <!-- todo hooks: must update setup.cfg, for version -->
 
+<!-- Todo standardize module & entry docstring for all dify apps' code node -->
 <!-- TODO kyc: improve task prompt content  -->
+<!-- Todo kyc: difficulty cool down, keep a array of difficult of recent rounds  -->
 <!-- FIXME kyc: pre-sense corpus, provide anchor point-->
 
 [^format]
--- dify_studio/kaye_peer_coder/nodes/parse_response_body.py
+++ dify_studio/kaye_peer_coder/nodes/parse_response_body.py
@@ -1,3 +1,6 @@
+# pylint: disable=missing-module-docstring
+
+
 import json
 
 # constants  ###################################################################
@@ -9,6 +12,17 @@ OUTPUT_PROMPT_FLAGS_KEY = "task_prompt_flags"
 
 # entry point  #################################################################
 def main(response_body: str):
+    """
+    parse response body from Kaye API into prompt and prompts' flags for caching
+
+
+    :param response_body: response body (typed `application/json`)
+            returned from Kaye API
+    :type response_body: str
+    :raises ValueError: _description_
+    :return: {"task_prompt": ~, "task_prompt_flags": ~}
+    :rtype: dict{"task_prompt": str, "task_prompt_flags": int}
+    """
     try:
         body_dict = json.loads(response_body)
         prompt = body_dict[BODY_PROMPT_KEY]
