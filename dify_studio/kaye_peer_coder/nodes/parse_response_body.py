8111ec6f2d8f5ca4f4e5ac9baddf6224014f7904
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-09 08:39:51 -0800

    update prompting

--- dify_studio/kaye_peer_coder/nodes/parse_response_body.py
+++ dify_studio/kaye_peer_coder/nodes/parse_response_body.py
@@ -1,10 +1,19 @@
+import json
+
+# constants  ###################################################################
+BODY_PROMPT_KEY = "prompt"
+BODY_FLAGS_KEY = "flags"
 OUTPUT_PROMPT_KEY = "task_prompt"
 OUTPUT_PROMPT_FLAGS_KEY = "task_prompt_flags"
 
 
+# entry point  #################################################################
 def main(response_body: str):
-    # TODO
-    prompt = ""
-    flags = 5
+    try:
+        body_dict = json.loads(response_body)
+        prompt = body_dict[BODY_PROMPT_KEY]
+        flags = body_dict[BODY_FLAGS_KEY]
+        return {OUTPUT_PROMPT_KEY: prompt, OUTPUT_PROMPT_FLAGS_KEY: flags}
 
-    return {OUTPUT_PROMPT_KEY: prompt, OUTPUT_PROMPT_FLAGS_KEY: flags}
+    except json.JSONDecodeError as err:
+        raise ValueError("fail to decode response_body as JSON") from err
