e802fc55286865a16a25b937156da79df16f9689
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-14 23:09:47 -0800

    add uncheckmark to SpellCheck Words in VSCode settings add test: PromptBlueprint.uncheckmark method, validate output, print keys, compare preview tree to expected, test with Project Title node
    [/]settings.json: Add 'uncheckmark' to VSCode settings extensions list
    [|]prompt_blueprint_remove_test.py: modify PromptBlueprint.remove() test to uncheckmark node, add assertions, print keys and blueprint preview tree for validation

--- tests/gen_prompt/blueprint/prompt_blueprint_remove_test.py
+++ tests/gen_prompt/blueprint/prompt_blueprint_remove_test.py
@@ -6,21 +6,33 @@ Unit Tests (using pytest) for: PromptBlueprint.remove()
 
 from kaye.gen_prompt import PromptCorpusNode, PromptBlueprint
 from tests.gen_prompt import PROMPT1, PROMPT2, PROMPT3
-from tests.gen_prompt.blueprint import (
-    BLUEPRINT_1_FULL,
-)
+from tests.gen_prompt.blueprint import BLUEPRINT_1_FULL, BLUEPRINT_1_PARTIAL_1
 
-# TODO
+# BUG
 
 
 class Test1:  # use corpus1  ###################################################
 
     corpus = PromptCorpusNode.parse(PROMPT1)
 
-    def test_obj1(self):
+    def test1_use_obj1(self):
         bp_text = BLUEPRINT_1_FULL
 
-        bp = PromptBlueprint.parse(self.corpus, bp_text, disable_prune=True)
+        opt = PromptBlueprint.parse(self.corpus, bp_text, disable_prune=True)
+
+        description_node = self.corpus["Project Title"]
+        print(opt.keys())
+
+        opt.uncheckmark(description_node)
+
+        print(opt.keys())
+        print(opt)
+        assert (
+            opt.generate_preview_tree(
+                preview_line_count=0, hide_comment=True, show_full_tree=True
+            )
+            == BLUEPRINT_1_PARTIAL_1
+        )
 
 
 class Test2:  # use corpus2  ##############################################
