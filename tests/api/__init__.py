899758516146e19e20ea08735e6c6a05df169f81
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-15 22:41:09 -0800

    remove HTTP_API_IP, HTTP_API_PORT vars from __init__.py; add api_dify_kaye_commit_sense_test.py skeleton, basic primary_message test
    [\]__init__.py: remove unused HTTP_API_IP and HTTP_API_PORT constants from api tests init to clean code
    [/]api_dify_kaye_commit_sense_test.py: Add pytest unit test for /kaye/dify-app/kaye-commit-sense/primary-message endpoint; check response contains 'a'

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -8,7 +8,3 @@ def flask_test_client():
     app = create_app()
     app.config["TESTING"] = True
     return app.test_client()
-
-
-HTTP_API_IP = "127.0.0.1"
-HTTP_API_PORT = 11255
--- tests/api/dify/api_dify_kaye_commit_sense_test.py
+++ tests/api/dify/api_dify_kaye_commit_sense_test.py
@@ -1,1 +1,13 @@
-# TODO
+"""
+api_dify_kaye_commit_sense_test.py
+
+Unit Tests (using pytest) for: /kaye/dify-app/kaye-commit-sense/*
+"""
+
+APP_PREFIX = "/kaye/dify-app/kaye-commit-sense"
+
+
+def test_primary_message(flask_test_client):
+    # BUG
+    response = flask_test_client.get(APP_PREFIX + "/primary-message")
+    assert "a" in response
