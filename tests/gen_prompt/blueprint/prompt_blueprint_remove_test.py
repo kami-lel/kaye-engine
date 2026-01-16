ff433c6fc901caf8e09bb6881dddd1360005c5d0
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-15 16:28:45 -0800

    add print helpers and debug output to test PromptBlueprint.remove() uncheckmark feature add test coverage for partial blueprint generation add spacing and comments for clarity in test methods
    [|]prompt_blueprint_remove_test.py

--- tests/gen_prompt/blueprint/prompt_blueprint_remove_test.py
+++ tests/gen_prompt/blueprint/prompt_blueprint_remove_test.py
@@ -4,6 +4,8 @@ prompt_blueprint_remove_test.py
 Unit Tests (using pytest) for: PromptBlueprint.remove()
 """
 
+from math import floor, ceil
+
 from kaye.gen_prompt import PromptCorpusNode, PromptBlueprint
 from tests.gen_prompt import PROMPT1, PROMPT2, PROMPT3
 from tests.gen_prompt.blueprint import BLUEPRINT_1_FULL, BLUEPRINT_1_PARTIAL_1
@@ -13,19 +15,22 @@ class Test1:  # use corpus1  ###################################################
 
     corpus = PromptCorpusNode.parse(PROMPT1)
 
-    def test1_use_obj1(self):
+    # full -> partial 1  -------------------------------------------------------
+    def test1_use_obj(self):
         bp_text = BLUEPRINT_1_FULL
 
         opt = PromptBlueprint.parse(self.corpus, bp_text, disable_prune=True)
+        _print_title("before uncheckmark")
 
         description_node = self.corpus["Project Title"]
 
-        print("=" * 15 + "  a  " + "=" * 15)
-
-        opt.uncheckmark(description_node)
-
-        print(opt.keys())
+        _print_title("after uncheckmark")
+        ret = opt.uncheckmark(description_node)
         print(opt)
+
+        _print_title("returned object")
+        print(ret)
+
         assert (
             opt.generate_preview_tree(
                 preview_line_count=0, hide_comment=True, show_full_tree=True
@@ -33,14 +38,38 @@ class Test1:  # use corpus1  ###################################################
             == BLUEPRINT_1_PARTIAL_1
         )
 
-    # TODO more
+        assert (
+            ret.generate_preview_tree(
+                preview_line_count=0, hide_comment=True, show_full_tree=True
+            )
+            == BLUEPRINT_1_PARTIAL_1
+        )
+
+    # full -> partial 2  -------------------------------------------------------
+    # TODO more tests
 
 
 class Test2:  # use corpus2  ##############################################
 
     corpus = PromptCorpusNode.parse(PROMPT2)
 
+    # full -> partial 1  -------------------------------------------------------
+
 
 class Test3:  # use corpus3  ##############################################
 
     corpus = PromptCorpusNode.parse(PROMPT3)
+
+    # full -> partial 1  -------------------------------------------------------
+    # full -> partial 2  -------------------------------------------------------
+
+
+# helper  ######################################################################
+
+
+def _print_title(content):
+    filler_length = (76 - len(content)) / 2
+    FILLER = "#"
+    print(
+        FILLER * ceil(filler_length) + content + FILLER * floor(filler_length)
+    )
