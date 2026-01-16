762d50fa7438c97f606c1c19811d55d069bd8ce0
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-16 01:59:15 -0800

    Refactor api_dify_kyc_test.py to class-based TestChat for /chat endpoint tests Add tests for no param and empty param cases Use helper _deconstruct_chat_response to parse JSON response Add debug print statements for flags and prompt Remove unused CHAT_ENDPOINT constant
    [|]api_dify_kyc_test.py

--- tests/api/dify/api_dify_kyc_test.py
+++ tests/api/dify/api_dify_kyc_test.py
@@ -4,6 +4,8 @@ api_dify_kyc_test.py
 Unit Tests (using pytest) for: /kaye/dify-app/kyc/*
 """
 
+import json
+
 from kaye.gen_prompt import (
     PromptBlueprint,
     load_embedded_prompt_corpus,
@@ -11,10 +13,11 @@ from kaye.gen_prompt import (
 from api.dify_app.kaye_peer_coder import (
     PRE_SENSE_PROMPT_BLUEPRINT,
     CHAT_PROMPT_BASIC_BLUEPRINT,
+    PL,
 )
+from tests import _print_heading
 
 APP_PREFIX = "/kaye/dify-app/kyc"
-CHAT_ENDPOINT = APP_PREFIX + "/chat"
 
 
 # test /pre-sense  #############################################################
@@ -35,19 +38,50 @@ def test_pre_sense(flask_test_client):
 # test /chat  ##################################################################
 
 
-def test_chat_basic(flask_test_client):
-    response = flask_test_client.get(CHAT_ENDPOINT)
-    opt = response.data.decode("utf-8")
+class TestChat:  # /chat  ######################################################
 
-    opt = response.data.decode("utf-8")
-    print(opt)
+    endpoint = APP_PREFIX + "/chat"
+    basic_prompt = PromptBlueprint.parse(
+        load_embedded_prompt_corpus(), CHAT_PROMPT_BASIC_BLUEPRINT
+    ).generate_prompt(hide_comment=True)
 
-    assert (
-        opt
-        == PromptBlueprint.parse(
-            load_embedded_prompt_corpus(), CHAT_PROMPT_BASIC_BLUEPRINT
-        ).generate_prompt()
-    )
+    def test_no_param(self, flask_test_client):
+        response = flask_test_client.get(self.endpoint)
+        flags, prompt = _deconstruct_chat_response(response)
+
+        _print_heading("flags")
+        print(flags)
+        _print_heading("prompt")
+        print(prompt)
+
+        assert flags == PL.NONE
+        assert self.basic_prompt in prompt
+
+    def test_empty_param(self, flask_test_client):
+        flags = PL.NONE
+        lang_param = ""
+
+        flags_param = int(flags)
+        response = flask_test_client.get(
+            self.endpoint
+            + "?flags={}&languages={}".format(flags_param, lang_param)
+        )
+        flags, prompt = _deconstruct_chat_response(response)
+
+        _print_heading("flags")
+        print(flags)
+        _print_heading("prompt")
+        print(prompt)
+
+        assert flags == PL.NONE
+        assert self.basic_prompt in prompt
 
 
-# TODO test params
+# helper  ######################################################################
+def _deconstruct_chat_response(response):
+    content = response.data.decode("utf-8")
+    data = json.loads(content)
+    flags = data["flags"]
+    prompt = data["prompt"]
+    print(prompt)  # HACK
+    return flags, prompt
