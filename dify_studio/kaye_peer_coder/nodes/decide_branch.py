2a05ef59e9c00fe8f96e7bbdde3c38d600afe71a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-17 11:16:10 -0800

    add decide_branch node stub with output constants and main
    ^`decide_branch.py`

---
+++ dify_studio/kaye_peer_coder/nodes/decide_branch.py
@@ -0,0 +1,16 @@
+# constants  ###################################################################
+OUTPUT_BRANCH_KEY = "branch"
+OUTPUT_PREFIX_KEY = "prefix_content"
+
+
+# Entry Point  #################################################################
+
+
+def main(
+    difficulty: dict, difficulty_thresholds: list[float], show_prefix_run_data
+):
+    branch = 0
+    prefix_content = ""
+    # TODO return metadata
+
+    return {OUTPUT_BRANCH_KEY: branch, OUTPUT_PREFIX_KEY: prefix_content}
