97c5a3653710e092e4f4d50454d286eb725742bb
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-18 00:48:59 -0800

    refactor branch decision to int difficulty, add historical messages and prefix
    ~`pre_branch.py`

--- dify_studio/kaye_peer_coder/nodes/pre_branch.py
+++ dify_studio/kaye_peer_coder/nodes/pre_branch.py
@@ -1,27 +1,47 @@
 """
-decide which branch/LLM to use for branching, also generate prefix meta content
+- decide which branch/LLM to use depending on difficulty
+
+- produce supplementary historical user/bot messages for this round
+  (exclude this round's user message)
+
+- produce prefix meta content
 
 
 :param difficulty:
-:type difficulty: float
-:param difficulty_thresholds: a list of thresholds, exclude 0.0 & 1.0
+:type difficulty: int
+:param difficulty_thresholds:
 :type difficulty_thresholds: list[float]
-:param show_prefix_meta_content:
-:type show_prefix_meta_content: bool
 :param languages:
 :type languages: str
+:param dialogue_count:
+:type dialogue_count: int
+:param dialogue_count:
+:type last_memory: list[int]
+:param historical_user_messages:
+:type historical_user_messages: list[str]
+:param historical_bot_messages:
+:type historical_bot_messages: list[str]
+:param show_prefix_meta_content:
+:type show_prefix_meta_content: bool
 :return: {
-        "branch": 0~2, which LLM/branch should be used
-        "prefix_meta_content": may be empty
+        "branch": 0~2, which LLM to be used
+        "supplement_user_messages": additional historical user messages
+                that this model is unaware of
+        "supplement_bot_messages": v.s.
+        "prefix_meta_content": prefix meta content, may be empty
         }
 :rtype: dict{
-        "branch": int
-        "prefix_meta_content": str
+        "branch": int,
+        "supplement_user_messages": str,
+        "supplement_bot_messages": str,
+        "prefix_meta_content": str,
         }
 """
 
 # constants  ###################################################################
 OUTPUT_BRANCH_KEY = "branch"
+OUTPUT_USER_KEY = "supplement_user_messages"
+OUTPUT_BOT_KEY = "supplement_bot_messages"
 OUTPUT_PREFIX_KEY = "prefix_meta_content"
 
 
@@ -29,12 +49,16 @@ OUTPUT_PREFIX_KEY = "prefix_meta_content"
 
 
 def main(
-    difficulty: float,
+    difficulty: int,
     difficulty_thresholds: list[float],
-    show_prefix_meta_content,
     languages: str,
+    dialogue_count: int,
+    last_memory: list[int],
+    historical_user_messages: list[str],
+    historical_bot_messages: list[str],
+    show_prefix_meta_content: bool,
 ):  # pylint: disable=missing-function-docstring
-    # decide branch  ------------------------------------------------------------
+    # decide branch  -----------------------------------------------------------
     if difficulty < difficulty_thresholds[0]:
         branch = 0
     elif difficulty < difficulty_thresholds[1]:
@@ -42,7 +66,12 @@ def main(
     else:
         branch = 2
 
-    # decide prefix    ---------------------------------------------------------
+    # generate historical messages  --------------------------------------------
+    # TODO
+    historical_user_message = ""
+    historical_bot_message = ""
+
+    # decide prefix  -----------------------------------------------------------
     prefix_content = ""
     if show_prefix_meta_content:
         prefix_content = """> difficulty = {}
@@ -52,4 +81,9 @@ def main(
 
 """.format(difficulty, languages, branch)
 
-    return {OUTPUT_BRANCH_KEY: branch, OUTPUT_PREFIX_KEY: prefix_content}
+    return {
+        OUTPUT_BRANCH_KEY: branch,
+        OUTPUT_USER_KEY: historical_user_message,
+        OUTPUT_BOT_KEY: historical_bot_message,
+        OUTPUT_PREFIX_KEY: prefix_content,
+    }
