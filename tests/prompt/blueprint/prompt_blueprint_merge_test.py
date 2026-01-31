6dd651ec6977bc9132bfe35fe08e893c8b901e62
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-30 20:13:53 -0800

    add tests for PromptBlueprint merge and in-place merge behavior
    *`prompt_blueprint_merge_test.py`

--- tests/prompt/blueprint/prompt_blueprint_merge_test.py
+++ tests/prompt/blueprint/prompt_blueprint_merge_test.py
@@ -14,19 +14,26 @@ from tests.prompt import PROMPT1, PROMPT3
 from tests.prompt.blueprint import (
     BLUEPRINT_1_PARTIAL_1,
     BLUEPRINT_3_PARTIAL_1,
-    BLUEPRINT_3_PARTIAL_1_PREVIEW,
     BLUEPRINT_3_PARTIAL_1_PRUNED,
     BLUEPRINT_3_PARTIAL_2,
-    BLUEPRINT_3_PARTIAL_2_PREVIEW,
     BLUEPRINT_3_PARTIAL_2_PRUNED,
     BLUEPRINT_3_EMPTY,
+    BLUEPRINT_3_FULL,
 )
 
 CORPUS1 = PromptCorpusNode.parse(PROMPT1)
 CORPUS3 = PromptCorpusNode.parse(PROMPT3)
 BP1_PARTIAL1 = PromptBlueprint.parse(CORPUS1, BLUEPRINT_1_PARTIAL_1)
 BP3_PARTIAL1 = PromptBlueprint.parse(CORPUS3, BLUEPRINT_3_PARTIAL_1)
+BP3_PARTIAL1_PRUNED = PromptBlueprint.parse(
+    CORPUS3, BLUEPRINT_3_PARTIAL_1_PRUNED
+)
 BP3_PARTIAL2 = PromptBlueprint.parse(CORPUS3, BLUEPRINT_3_PARTIAL_2)
+BP3_PARTIAL2_PRUNED = PromptBlueprint.parse(
+    CORPUS3, BLUEPRINT_3_PARTIAL_2_PRUNED
+)
+BP3_FULL = PromptBlueprint.parse(CORPUS3, BLUEPRINT_3_FULL)
+BP3_EMPTY = PromptBlueprint.parse(CORPUS3, BLUEPRINT_3_EMPTY)
 
 # test .merge()  ###############################################################
 
@@ -36,7 +43,93 @@ class TestMerge1:
     left_bp = BP3_PARTIAL1
     right_bp = BP3_PARTIAL2
 
-    # TODO TODO
+    def test_merge1(self):
+        merged = self.left_bp.merge(self.right_bp)
+
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == """    ○
+[x] └── Main Title
+[x]     ├── Introduction
+[x]     │   └── Background
+[x]     │       └── Importance
+[x]     │           └── Objective
+[ ]     ├── Methods
+[x]     │   └── Data Collection
+[ ]     │       └── Tools Used
+[x]     │           └── Future Work
+[x]     └── Conclusion"""
+        )
+
+    def test_merge2(self):
+        merged = BP3_PARTIAL1_PRUNED.merge(BP3_PARTIAL2_PRUNED)
+
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == """    ○
+[x] └── Main Title
+[x]     ├── Introduction
+[x]     │   └── Background
+[x]     │       └── Importance
+[x]     │           └── Objective
+[ ]     ├── Methods
+[x]     │   └── Data Collection
+[ ]     │       └── Tools Used
+[x]     │           └── Future Work
+[x]     └── Conclusion"""
+        )
+
+    def test_empty1(self):
+        # BUG BUG
+        merged = BP3_EMPTY.merge(self.right_bp)
+
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == BLUEPRINT_3_PARTIAL_2
+        )
+
+    def test_empty2(self):
+        # BUG BUG
+        merged = self.right_bp.merge(BP3_EMPTY)
+
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == BLUEPRINT_3_PARTIAL_2
+        )
+
+    def test_full1(self):
+        merged = BP3_FULL.merge(self.right_bp)
+
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == BLUEPRINT_3_FULL
+        )
+
+    def test_full2(self):
+        merged = self.right_bp.merge(BP3_FULL)
+
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == BLUEPRINT_3_FULL
+        )
 
     # err handling  ************************************************************
     def test_bad_type(self):
@@ -78,6 +171,47 @@ class TestIMul:
     left_bp = BP3_PARTIAL1
     right_bp = BP3_PARTIAL2
 
-    # TODO TODO
+    def test_merge1(self):
+        merged = self.left_bp.copy()
+        merged *= self.right_bp
 
-    pass
+        print(merged)
+        assert (
+            merged.generate_preview_tree(
+                preview_line_count=0, hide_comment=True
+            )
+            == """    ○
+[x] └── Main Title
+[x]     ├── Introduction
+[x]     │   └── Background
+[x]     │       └── Importance
+[x]     │           └── Objective
+[ ]     ├── Methods
+[x]     │   └── Data Collection
+[ ]     │       └── Tools Used
+[x]     │           └── Future Work
+[x]     └── Conclusion"""
+        )
+
+    # err handling  ************************************************************
+    def test_bad_type(self):
+        bp = self.left_bp.copy()
+
+        with pytest.raises(ValueError) as exec_info:
+            bp *= BP1_PARTIAL1
+
+        opt = exec_info.value.args[0]
+        print(opt)
+
+        assert opt == "must merge 2 blueprints with same corpus"
+
+    def test_bad_corpus(self):
+        bp = self.left_bp.copy()
+
+        with pytest.raises(TypeError) as exec_info:
+            bp *= 15.0
+
+        opt = exec_info.value.args[0]
+        print(opt)
+
+        assert opt == "must merge another PromptBlueprint, not: 15.0"
