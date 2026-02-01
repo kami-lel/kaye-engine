6e169508a0a3bbdf65b68d393f7a29878251999c
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-31 19:52:01 -0800

    refactor dynamic abbr blueprint: scaffold prompt logic, prep for abbr type handling
    @`python_api_doc.md` clean duplicate TODO in dynamic abbreviation blueprint doc
    /`dynamic_abbr_blueprint.py` add DynamicAbbrBlueprint prompt generation stub with abbr placeholder

--- kaye/gen_prompt/dynamic_abbr_blueprint.py
+++ kaye/gen_prompt/dynamic_abbr_blueprint.py
@@ -3,10 +3,17 @@ from .prompt_blueprint import PromptBlueprint
 
 class DynamicAbbrBlueprint(PromptBlueprint):
 
-    def generate_prompt(self, *, hide_comment=False, query=None):
-
-        pass
-
-        # TODO TODO abbr type (how to interpret the abbr)
-
     # TODO always/chat abbr type? always provided during chat
+
+    def generate_prompt(self, *, hide_comment=False, query=None):
+        content, comment = self._generate_prompt_split_content_and_comment(
+            hide_comment
+        )
+
+        if query:
+            # TODO TODO abbr type (how to interpret the abbr)
+            abbr_content = ""
+        else:
+            abbr_content = ""
+
+        return content + abbr_content + comment
