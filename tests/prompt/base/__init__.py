d1514a75aab28a8e0b022974486ee993a56620c4
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-02-04 21:31:37 -0800

    introduce UnitTestNode, WORLD hierarchy; add lineage tests for prompt nodes
    ^`__init__.py` add UnitTestNode class and hierarchical nodes to base prompt tests
    ^`prompt_base_lineage_test.py` add unit tests for BasePromptNode.generate_lineage method

--- tests/prompt/base/__init__.py
+++ tests/prompt/base/__init__.py
@@ -1,1 +1,39 @@
 from kaye.gen_prompt.base_prompt_node import BasePromptNode
+
+__all__ = [
+    "UnitTestNode",
+    "WORLD",
+    "MOUNTAIN",
+    "PEAK",
+    "CAVE",
+    "LAKE",
+    "FOREST",
+    "GLADE",
+    "STREAM",
+    "VILLAGE",
+    "MARKET",
+]
+
+
+class UnitTestNode(BasePromptNode):
+
+    @property
+    def name_in_lineage(self):
+        return self.name.upper()
+
+    @property
+    def content_lines(self):
+        return self.content_lines
+
+
+# Node declarations outside class with ALL CAPS names
+WORLD = UnitTestNode("WORLD")
+MOUNTAIN = UnitTestNode("MOUNTAIN", parent=WORLD)
+PEAK = UnitTestNode("PEAK", parent=MOUNTAIN)
+CAVE = UnitTestNode("CAVE", parent=PEAK)
+LAKE = UnitTestNode("LAKE", parent=MOUNTAIN)
+FOREST = UnitTestNode("FOREST", parent=WORLD)
+GLADE = UnitTestNode("GLADE", parent=FOREST)
+STREAM = UnitTestNode("STREAM", parent=FOREST)
+VILLAGE = UnitTestNode("VILLAGE", parent=WORLD)
+MARKET = UnitTestNode("MARKET", parent=VILLAGE)
--- tests/prompt/base/prompt_base_lineage_test.py
+++ tests/prompt/base/prompt_base_lineage_test.py
@@ -6,4 +6,67 @@ Unit Tests (using pytest) for:
 - BasePromptNode.generate_lineage()
 """
 
-# TODO
+from tests.prompt.base import *
+
+
+class TestGenLineage:
+
+    def test_root(self):
+        node = WORLD
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == [""]
+
+    def test_mountain(self):
+        node = MOUNTAIN
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "MOUNTAIN"]
+
+    def test_peak(self):
+        node = PEAK
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "MOUNTAIN", "PEAK"]
+
+    def test_cave(self):
+        node = CAVE
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "MOUNTAIN", "PEAK", "CAVE"]
+
+    def test_lake(self):
+        node = LAKE
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "MOUNTAIN", "LAKE"]
+
+    def test_forest(self):
+        node = FOREST
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "FOREST"]
+
+    def test_glade(self):
+        node = GLADE
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "FOREST", "GLADE"]
+
+    def test_stream(self):
+        node = STREAM
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "FOREST", "STREAM"]
+
+    def test_village(self):
+        node = VILLAGE
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "VILLAGE"]
+
+    def test_market(self):
+        node = MARKET
+        opt = node.generate_lineage()
+        print(opt)
+        assert opt == ["", "VILLAGE", "MARKET"]
