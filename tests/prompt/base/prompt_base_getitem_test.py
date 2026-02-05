c4d253f431b1e2e32ae4be708c8f4052b56faf94
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-02-04 22:08:01 -0800

    add pytest tests for BasePromptNode __getitem__ bad type errors
    /`prompt_base_getitem_test.py`

--- tests/prompt/base/prompt_base_getitem_test.py
+++ tests/prompt/base/prompt_base_getitem_test.py
@@ -6,4 +6,42 @@ Unit Tests (using pytest) for:
 BasePromptNode.__getitem__()
 """
 
-# TODO
+import pytest
+
+from tests.prompt.base import *
+
+
+class TestByInt:  ##############################################################
+    pass  # TODO
+
+
+class TestByStr:  ##############################################################
+    pass  # TODO
+
+
+# bad type  ####################################################################
+
+
+class TestBadType:
+
+    def test1(_):
+        key = []
+        node = WORLD
+
+        with pytest.raises(TypeError) as exec_info:
+            node[key]
+        opt = exec_info.value.args[0]
+
+        print(opt)
+        assert opt == "UnitTestNode index must be int/str: []"
+
+    def test2(_):
+        key = {"a": 5}
+        node = FOREST
+
+        with pytest.raises(TypeError) as exec_info:
+            node[key]
+        opt = exec_info.value.args[0]
+
+        print(opt)
+        assert opt == "UnitTestNode index must be int/str: {'a': 5}"
