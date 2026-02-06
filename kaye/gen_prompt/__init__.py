e0b431e91fd99299621e52a6faa26bc3fffa6f33
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-02-06 11:17:31 -0800

    add todo note to remove package layer under kaye
    #`__init__.py`

--- kaye/gen_prompt/__init__.py
+++ kaye/gen_prompt/__init__.py
@@ -13,3 +13,5 @@ from .prompt_blueprint import *
 from .prompt_blueprint_loader import *
 from .abbr_collection import *
 from .abbr_nodes import *
+
+# Todo remove this layer of package, directly place under kaye
