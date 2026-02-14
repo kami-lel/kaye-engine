abc3c7b8bb755fc1abd78a1896a305162bdb4cbc
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-02-13 16:34:52 -0800

    add comprehensive tests for AbbrNode functionality and copying
    ^`prompt-abbr_node-test.py`

--- tests/prompt/prompt-abbr_node-test.py
+++ tests/prompt/prompt-abbr_node-test.py
@@ -1,1 +1,61 @@
-# TODO TODO
+import copy
+
+
+import pytest
+
+
+from kaye.gen_prompt.abbr_nodes import AbbrNode
+
+
+# pytest fixtures  #############################################################
+@pytest.fixture(scope="session")
+def local_corpus_testee1(corpus_testee1):
+    return copy.deepcopy(corpus_testee1)
+
+
+@pytest.fixture(scope="session")
+def testee1(local_corpus_testee1):
+    return AbbrNode(local_corpus_testee1)
+
+
+class TestInit:  ###############################################################
+
+    def test1(_, testee1, local_corpus_testee1):
+        assert testee1.parent is local_corpus_testee1
+        assert testee1.name == "Abbreviations"
+        assert testee1.id == "{Abbreviations}"
+
+    def test_preview1(_, local_corpus_testee1):
+        opt = local_corpus_testee1.generate_prompt_tree_preview(
+            content_preview_lines=0
+        )
+        print(opt)
+        assert opt == """○
+├── Project Title
+│   ├── Description
+│   ├── Installation
+│   └── License
+└── Abbreviations"""
+
+
+class TestCopy:  ###############################################################
+
+    def test_copy1(_, local_corpus_testee1, testee1):
+        copied = copy.copy(testee1)
+
+        assert isinstance(copied, AbbrNode)
+        assert copied.name == "Abbreviations"
+        assert copied.parent is local_corpus_testee1
+
+    def test_deep_copy1(_, local_corpus_testee1, testee1):
+        copied = copy.deepcopy(testee1)
+
+        assert isinstance(copied, AbbrNode)
+        assert copied.name == "Abbreviations"
+        assert copied.parent is local_corpus_testee1
+
+
+class TestContentLines:  #######################################################
+
+    def test1(_):
+        pass  # Todo abbr node functionality test
