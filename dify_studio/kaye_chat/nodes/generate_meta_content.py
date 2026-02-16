50a28c7893ccb8f59f324ab553bb51abca427462
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-02-16 03:48:09 -0800

    add initial generate_meta_content node with meta content template
    ^`generate_meta_content.py`

---
+++ dify_studio/kaye_chat/nodes/generate_meta_content.py
@@ -0,0 +1,29 @@
+# pylint: disable=missing-module-docstring
+
+
+# output keys  #################################################################
+OUTPUT_META_KEY = "meta_content"
+
+
+# constants  ###################################################################
+USAGE_TIME_KEY = "time_to_generate"
+
+
+# Entry Point  #################################################################
+def main(
+    show_meta_content,
+    pre_sense_usage,
+):
+    if not show_meta_content:
+        return {OUTPUT_META_KEY: ""}  # skip
+
+    lines = []
+    # TODO
+
+    # meta content form  -------------------------------------------------------
+    meta_content = """
+
+> [!TIP]
+""" + "\n".join("> " + line for line in lines)
+
+    return {OUTPUT_META_KEY: meta_content}
