6f017660ba34f931ccb3bf494dbf83a310c0993c
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-02-16 20:08:18 -0800

    refactor prompt-bp-is_checkmarked_test.py to use pytest fixtures
    ~`prompt-bp-is_checkmarked_test.py`

--- tests/prompt/bp/prompt-bp-is_checkmarked_test.py
+++ tests/prompt/bp/prompt-bp-is_checkmarked_test.py
@@ -7,6 +7,9 @@ Unit Tests (using pytest) for: PromptBlueprint.is_checkmarked()
 import copy
 
 
+import pytest
+
+
 from kaye.prompt import PromptBlueprint
 from tests.prompt.bp import (
     BLUEPRINT_1_FULL,
@@ -20,167 +23,215 @@ from tests.prompt.bp import (
 )
 
 
-class Test1:  # use corpus1  ###################################################
+# use corpus 1  ################################################################
+# pytest fixtures  =============================================================
+@pytest.fixture(scope="class")
+def bp_testee1full(corpus_testee1):
+    corpus = corpus_testee1
+    return PromptBlueprint.parse(
+        BLUEPRINT_1_FULL, disable_prune=True, corpus_override=corpus
+    )
 
-    def test_full(_, corpus_testee1):
+
+@pytest.fixture(scope="class")
+def bp_testee1pa1(corpus_testee1):
+    corpus = corpus_testee1
+    return PromptBlueprint.parse(
+        BLUEPRINT_1_PARTIAL_1, disable_prune=True, corpus_override=corpus
+    )
+
+
+@pytest.fixture(scope="class")
+def bp_testee1pa2(corpus_testee1):
+    corpus = corpus_testee1
+    return PromptBlueprint.parse(
+        BLUEPRINT_1_PARTIAL_2, disable_prune=True, corpus_override=corpus
+    )
+
+
+@pytest.fixture(scope="class")
+def bp_testee1empty(corpus_testee1):
+    corpus = corpus_testee1
+    return PromptBlueprint.parse(
+        BLUEPRINT_1_EMPTY, disable_prune=True, corpus_override=corpus
+    )
+
+
+class Test1Full:  # ============================================================
+
+    def test_proj(_, corpus_testee1, bp_testee1full):
+        bp = bp_testee1full
         corpus = corpus_testee1
-        bp_text = BLUEPRINT_1_FULL
-
-        bp = PromptBlueprint.parse(
-            bp_text, disable_prune=True, corpus_override=corpus
-        )
-
-        # test entries  --------------------------------------------------------
-        # test Project Title
         proj_node = corpus.children[0]
 
         opt = bp.is_checkmarked(proj_node)
         print(repr(opt) + "\t" + repr(proj_node))
         assert opt
 
-        # test Description
-        _node = proj_node.children[0]
-        print(repr(opt) + "\t" + repr(proj_node))
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-        # test Installation
-        _node = proj_node.children[1]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-        # test License
-        _node = proj_node.children[2]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-    def test_no_project(_, corpus_testee1):
+    def test_description(_, corpus_testee1, bp_testee1full):
+        bp = bp_testee1full
         corpus = corpus_testee1
-        bp_text = BLUEPRINT_1_PARTIAL_1
+        node = corpus.children[0].children[0]
 
-        bp = PromptBlueprint.parse(
-            bp_text, disable_prune=True, corpus_override=corpus
-        )
-
-        # test entries  --------------------------------------------------------
-        # test Project Title
-        proj_node = corpus.children[0]
-        opt = bp.is_checkmarked(proj_node)
-        print(repr(opt) + "\t" + repr(proj_node))
-        assert not opt
-
-        # test Description
-        _node = proj_node.children[0]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
         assert opt
 
-        # test Installation
-        _node = proj_node.children[1]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-        # test License
-        _node = proj_node.children[2]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-    def test_no_description(_, corpus_testee1):
+    def test_installation(_, corpus_testee1, bp_testee1full):
+        bp = bp_testee1full
         corpus = corpus_testee1
-        bp_text = BLUEPRINT_1_PARTIAL_2
+        node = corpus.children[0].children[1]
 
-        bp = PromptBlueprint.parse(
-            bp_text, disable_prune=True, corpus_override=corpus
-        )
-
-        print(bp)
-        opt = len(bp) == 4
-
-        # test entries  --------------------------------------------------------
-        # test Project Title
-        proj_node = corpus.children[0]
-        opt = bp.is_checkmarked(proj_node)
-        print(repr(opt) + "\t" + repr(proj_node))
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
         assert opt
 
-        # test Description
-        _node = proj_node.children[0]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert not opt
-
-        # test Installation
-        _node = proj_node.children[1]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-        # test License
-        _node = proj_node.children[2]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert opt
-
-    def test_empty(_, corpus_testee1):
+    def test_license(_, corpus_testee1, bp_testee1full):
+        bp = bp_testee1full
         corpus = corpus_testee1
-        bp_text = BLUEPRINT_1_EMPTY
+        node = corpus.children[0].children[2]
 
-        bp = PromptBlueprint.parse(
-            bp_text, disable_prune=True, corpus_override=corpus
-        )
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert opt
 
-        print(bp)
-        opt = len(bp) == 4
+    # special cases  -----------------------------------------------------------
 
-        # test entries  --------------------------------------------------------
-        # test Project Title
-        proj_node = corpus.children[0]
-        opt = bp.is_checkmarked(proj_node)
-        print(repr(opt) + "\t" + repr(proj_node))
-        assert not opt
-
-        # test Description
-        _node = proj_node.children[0]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert not opt
-
-        # test Installation
-        _node = proj_node.children[1]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert not opt
-
-        # test License
-        _node = proj_node.children[2]
-        opt = bp.is_checkmarked(_node)
-        print(repr(opt) + "\t" + repr(_node))
-        assert not opt
-
-    # fail cases  ==============================================================
-
-    # a node that is not contained in bp at all
-    def test_not_contained1(_, corpus_testee1, corpus_testee3):
-        bp = PromptBlueprint.parse(
-            BLUEPRINT_1_FULL,
-            disable_prune=True,
-            corpus_override=corpus_testee1,
-        )
+    def test_not_contained1(_, corpus_testee3, bp_testee1full):
+        # a node that is not contained in bp at all
+        bp = bp_testee1full
         assert not bp.is_checkmarked(corpus_testee3)
 
-    # a node that is not contained in bp at all
-    def test_not_contained2(_, corpus_testee1, corpus_testee3):
-        bp = PromptBlueprint.parse(
-            BLUEPRINT_1_FULL,
-            disable_prune=True,
-            corpus_override=corpus_testee1,
-        )
+    def test_not_contained2(_, corpus_testee3, bp_testee1full):
+        # a node that is not contained in bp at all
+        bp = bp_testee1full
         assert not bp.is_checkmarked(corpus_testee3.children[0])
+
+
+class Test1Pa1:  # =============================================================
+
+    def test_proj(_, corpus_testee1, bp_testee1pa1):
+        bp = bp_testee1pa1
+        corpus = corpus_testee1
+        proj_node = corpus.children[0]
+
+        opt = bp.is_checkmarked(proj_node)
+        print(repr(opt) + "\t" + repr(proj_node))
+        assert not opt
+
+    def test_description(_, corpus_testee1, bp_testee1pa1):
+        bp = bp_testee1pa1
+        corpus = corpus_testee1
+        node = corpus.children[0].children[0]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert opt
+
+    def test_installation(_, corpus_testee1, bp_testee1pa1):
+        bp = bp_testee1pa1
+        corpus = corpus_testee1
+        node = corpus.children[0].children[1]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert opt
+
+    def test_license(_, corpus_testee1, bp_testee1pa1):
+        bp = bp_testee1pa1
+        corpus = corpus_testee1
+        node = corpus.children[0].children[2]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert opt
+
+
+class Test1Pa2:  # =============================================================
+
+    def test_proj(_, corpus_testee1, bp_testee1pa2):
+        bp = bp_testee1pa2
+        corpus = corpus_testee1
+        proj_node = corpus.children[0]
+
+        opt = bp.is_checkmarked(proj_node)
+        print(repr(opt) + "\t" + repr(proj_node))
+        assert opt
+
+    def test_description(_, corpus_testee1, bp_testee1pa2):
+        bp = bp_testee1pa2
+        corpus = corpus_testee1
+        node = corpus.children[0].children[0]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert not opt
+
+    def test_installation(_, corpus_testee1, bp_testee1pa2):
+        bp = bp_testee1pa2
+        corpus = corpus_testee1
+        node = corpus.children[0].children[1]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert opt
+
+    def test_license(_, corpus_testee1, bp_testee1pa2):
+        bp = bp_testee1pa2
+        corpus = corpus_testee1
+        node = corpus.children[0].children[2]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert opt
+
+
+class Test1Empty:  # ===========================================================
+
+    def test_proj(_, corpus_testee1, bp_testee1empty):
+        bp = bp_testee1empty
+        corpus = corpus_testee1
+        proj_node = corpus.children[0]
+
+        opt = bp.is_checkmarked(proj_node)
+        print(repr(opt) + "\t" + repr(proj_node))
+        assert not opt
+
+    def test_description(_, corpus_testee1, bp_testee1empty):
+        bp = bp_testee1empty
+        corpus = corpus_testee1
+        node = corpus.children[0].children[0]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert not opt
+
+    def test_installation(_, corpus_testee1, bp_testee1empty):
+        bp = bp_testee1empty
+        corpus = corpus_testee1
+        node = corpus.children[0].children[1]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert not opt
+
+    def test_license(_, corpus_testee1, bp_testee1empty):
+        bp = bp_testee1empty
+        corpus = corpus_testee1
+        node = corpus.children[0].children[2]
+
+        opt = bp.is_checkmarked(node)
+        print(repr(opt) + "\t" + repr(node))
+        assert not opt
+
+
+# use corpus 3  ################################################################
+# pytest fixtures  =============================================================
+
+# use dynamic nodes  ###########################################################
+
+
+# HACK rm these
 
 
 class Test3:  # use corpus3  ##################################################
