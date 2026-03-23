607030111f062a048221eb23c2dbcbff6f217ab9
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-23 11:28:31 -0700

    add pre_task_prompt_getter node to create JSON GET body
    ^`pre_task_prompt_getter.py`

---
+++ dify_studio/kaye_chat/nodes/pre_task_prompt_getter.py
@@ -0,0 +1,34 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+
+
+import json
+
+# output keys  #################################################################
+OUTPUT_BODY_KEY = "body"
+
+
+# Entry Point  #################################################################
+def main(query: str, role: str, programming_languages: str):
+    """
+    create a json-typed GET body for task prompt getter
+
+
+    :param query:
+    :type query: str
+    :param role:
+    :type role: str
+    :param programming_languages:
+    :type programming_languages: str
+    :return: {"body": generated JSON GET body}
+    :rtype: dict{"body": str}
+    """
+
+    body_content = {
+        "query": query,
+        "role": role,
+        "programming_languages": programming_languages,
+    }
+    body = json.dumps(body_content)
+
+    return {OUTPUT_BODY_KEY: body}
