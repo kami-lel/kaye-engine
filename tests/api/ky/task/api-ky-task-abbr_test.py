08dc32db34109d6059e093b856d786f9fe2aba3d
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-23 17:29:28 -0700

    add multiple role-based tests for api-ky-task abbreviation endpoint
    *`api-ky-task-abbr_test.py`

--- tests/api/ky/task/api-ky-task-abbr_test.py
+++ tests/api/ky/task/api-ky-task-abbr_test.py
@@ -22,7 +22,7 @@ def _assert_abbreviations_heading(opt):
 
 class TestSingle:  # ===========================================================
 
-    def test1(self, flask_test_client, task_endpoint):
+    def test_chat(self, flask_test_client, task_endpoint):
         query = "afx"
         payload = {"role": "chat", "query": query}
 
@@ -40,10 +40,98 @@ class TestSingle:  # ===========================================================
         _assert_abbreviations_heading(opt)
         assert "- afx:affect,affected,affectedly,affectation" in opt
 
-    # TODO more unit test
+    def test_rapid(self, flask_test_client, task_endpoint):
+        query = "afx"
+        payload = {"role": "rapid", "query": query}
+
+        response = flask_test_client.get(
+            task_endpoint,
+            data=json.dumps(payload),
+            content_type="application/json",
+        )
+
+        opt = response.get_data().decode("utf-8")
+
+        print(opt)
+
+        _assert_rapid_blueprint_opt(opt)
+        _assert_abbreviations_heading(opt)
+        assert "- afx:affect,affected,affectedly,affectation" in opt
+
+    def test_coder(self, flask_test_client, task_endpoint):
+        query = "afx"
+        payload = {"role": "coder", "query": query}
+
+        response = flask_test_client.get(
+            task_endpoint,
+            data=json.dumps(payload),
+            content_type="application/json",
+        )
+
+        opt = response.get_data().decode("utf-8")
+
+        print(opt)
+
+        _assert_rapid_blueprint_opt(opt)
+        _assert_abbreviations_heading(opt)
+        assert "- afx:affect,affected,affectedly,affectation" in opt
+
+    def test_barista(self, flask_test_client, task_endpoint):
+        query = "afx"
+        payload = {"role": "barista", "query": query}
+
+        response = flask_test_client.get(
+            task_endpoint,
+            data=json.dumps(payload),
+            content_type="application/json",
+        )
+
+        opt = response.get_data().decode("utf-8")
+
+        print(opt)
+
+        _assert_rapid_blueprint_opt(opt)
+        _assert_abbreviations_heading(opt)
+        assert "- afx:affect,affected,affectedly,affectation" in opt
+
+    def test_editor(self, flask_test_client, task_endpoint):
+        query = "afx"
+        payload = {"role": "editor", "query": query}
+
+        response = flask_test_client.get(
+            task_endpoint,
+            data=json.dumps(payload),
+            content_type="application/json",
+        )
+
+        opt = response.get_data().decode("utf-8")
+
+        print(opt)
+
+        _assert_rapid_blueprint_opt(opt)
+        _assert_abbreviations_heading(opt)
+        assert "- afx:affect,affected,affectedly,affectation" in opt
+
+    def test_secretary(self, flask_test_client, task_endpoint):
+        query = "afx"
+        payload = {"role": "secretary", "query": query}
+
+        response = flask_test_client.get(
+            task_endpoint,
+            data=json.dumps(payload),
+            content_type="application/json",
+        )
+
+        opt = response.get_data().decode("utf-8")
+
+        print(opt)
+
+        _assert_rapid_blueprint_opt(opt)
+        _assert_abbreviations_heading(opt)
+        assert "- afx:affect,affected,affectedly,affectation" in opt
 
 
 class TestMux:  # ==============================================================
 
     def test1(_):
-        pass
+        pass  # TODO more unit test
