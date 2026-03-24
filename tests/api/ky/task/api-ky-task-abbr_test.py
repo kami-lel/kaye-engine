42f67668d67da4ef0887d9ca76030e0516f716ff
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-23 17:36:18 -0700

    add tests to verify abbreviations in mux options, highlight BUG
    #`api-ky-task-abbr_test.py`

--- tests/api/ky/task/api-ky-task-abbr_test.py
+++ tests/api/ky/task/api-ky-task-abbr_test.py
@@ -149,4 +149,10 @@ class TestMux:  # ==============================================================
 
         _assert_rapid_blueprint_opt(opt)
         _assert_abbreviations_heading(opt)
+
+        assert "- achv:achieve,achieved,achievement" in opt
+        assert "- admin:administrate,administrator,administration" in opt
+        assert "- w/:with" in opt
+        assert "- aknlg:acknowledge" in opt
+
         assert False  # BUG mux insertion of abbrs
