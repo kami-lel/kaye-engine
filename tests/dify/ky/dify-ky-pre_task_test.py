748d4ebf27e42a66bf0b68491155fa059e967bc3
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-05 14:43:11 -0700

    tighten pre_task output assertions: types for body/llms/direct
    |`dify-ky-pre_task_test.py`

--- tests/dify/ky/dify-ky-pre_task_test.py
+++ tests/dify/ky/dify-ky-pre_task_test.py
@@ -20,10 +20,13 @@ from dify_studio.kaye_chat.nodes.pre_task import (
 
 
 def _assert_structure(opt):
-    # TODO check type
     assert OUTPUT_BODY_KEY in opt
+    assert isinstance(opt[OUTPUT_BODY_KEY], str)
     assert OUTPUT_LLMS_KEY in opt
+    assert isinstance(opt[OUTPUT_LLMS_KEY], list)
+    assert all(isinstance(e, str) for e in opt)
     assert OUTPUT_DIRECT_KEY in opt
+    assert isinstance(opt[OUTPUT_DIRECT_KEY], bool)
 
 
 # Pytest unit tests  ###########################################################
