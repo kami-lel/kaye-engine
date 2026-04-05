01b4e35958c07d896b2e138687644f5284b9bc6e
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-05 14:25:12 -0700

    consolidate task prompt getter into pre_task node
    /`pre_task.py` add JSON GET Body gen for Task Prompt Getter node
    !`pre_task_prompt_getter.py` \ remove pre_task_prompt_getter node HACK json body builder

--- dify_studio/kaye_chat/nodes/pre_task.py
+++ dify_studio/kaye_chat/nodes/pre_task.py
@@ -1,6 +1,9 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=too-many-arguments
 
+
+import json
+
 # Output Key ###################################################################
 OUTPUT_BODY_KEY = "body"
 OUTPUT_LLMS_KEY = "llms"
@@ -14,4 +17,33 @@ def main(
     current_pls: str,
     difficulty: float,
 ):
-    pass  # TODO
+    """
+    create a json-typed GET body for task prompt getter
+
+
+    :param query:
+    :type query: str
+    :param current_role:
+    :type current_role: str
+    :param current_pls:
+    :type current_pls: str
+    :param difficulty:
+    :type difficulty: float
+    :return:
+    :rtype:
+    TODO write docstring
+    """
+    # gen body  ================================================================
+    # used for Task Prompt Getter node
+
+    body = {
+        "query": query,
+        "role": current_role,
+        "programming_languages": current_pls,
+    }
+    body_json_dumps = json.dumps(body)
+
+    # gen LLMs  ================================================================
+    # TODO
+
+    return {OUTPUT_BODY_KEY: body_json_dumps}
