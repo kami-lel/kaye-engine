90f4c8243c15b5546b989234c68b23ee7ec5a39a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-07 22:38:47 -0700

    Add pytest tests for sense API endpoint validation
    ~`api-ky-sense-both_test.py`

--- tests/api/ky/sense/api-ky-sense-both_test.py
+++ tests/api/ky/sense/api-ky-sense-both_test.py
@@ -6,4 +6,45 @@ Unit Tests (using pytest) for:
 /kaye/dify-app/ky/sense to sense for role & difficulty
 """
 
-# TODO
+import pytest
+
+from tests.api.ky.sense import (
+    _assert_sense_title1,
+    _assert_sense_title2,
+    _assert_empty_pls1,
+    _assert_empty_pls2,
+)
+
+# Pytest fixtures  #############################################################
+
+
+@pytest.fixture(scope="class")
+def opt(flask_test_client, sense_endpoint):
+    request_body = {"pre_sense_role": "", "difficulty_override": 0}
+
+    response = flask_test_client.post(sense_endpoint, json=request_body)
+    return response.get_data().decode("utf-8")
+
+
+# Pytest unit tests  ###########################################################
+
+
+class TestBoth:
+
+    def test_sense_title1(_, opt):
+        print(opt)
+        _assert_sense_title1(opt)
+
+    def test_sense_title2(_, opt):
+        print(opt)
+        _assert_sense_title2(opt)
+
+    # TODO both
+
+    def test_empty_pls1(_, opt):
+        print(opt)
+        _assert_empty_pls1(opt)
+
+    def test_empty_pls2(_, opt):
+        print(opt)
+        _assert_empty_pls2(opt)
