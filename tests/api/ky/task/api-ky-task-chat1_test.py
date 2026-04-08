1f2b4cad051a25c7e488c4efa0d32d769df29502
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-08 00:02:16 -0700

    Add initial unit test skeleton for API Ky Task Chat1
    ^`api-ky-task-chat1_test.py`

---
+++ tests/api/ky/task/api-ky-task-chat1_test.py
@@ -0,0 +1,19 @@
+"""
+api-ky-task-chat1_test.py
+
+Unit Tests (using pytest) for:
+
+/kaye/dify-api/ky/task with:
+
+- role=chat
+- no PLs
+"""
+
+import json
+
+# Pytest unit tests  ###########################################################
+
+
+class TestChat:  # =============================================================
+
+    pass  # TODO
