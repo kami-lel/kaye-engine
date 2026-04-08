f7daf879d482019ff7654d5dccadd5decba6bee1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-07 22:19:27 -0700

    implement `TestDiff` class; fix `opt` fixture, drop `request_body`
    +`api-ky-sense-diff_test.py` Add tests for sense difficulty description and task levels
    ~`conftest.py` Remove unused fixture request_body in test config

--- tests/api/ky/sense/api-ky-sense-diff_test.py
+++ tests/api/ky/sense/api-ky-sense-diff_test.py
@@ -11,11 +11,41 @@ import pytest
 # Pytest fixtures  #############################################################
 
 
-@pytest.fixture
-def opt(request_body, flask_test_client, sense_endpoint):
-    return
+@pytest.fixture(scope="class")
+def opt(flask_test_client, sense_endpoint):
+    request_body = {"pre_sense_role": "chat", "difficulty_override": 0}
+    response = flask_test_client.post(sense_endpoint, json=request_body)
+    return response.get_data().decode("utf-8")
 
 
 # Pytest unit tests  ###########################################################
+
+
+class TestDiff:
+
+    def test_title1(_, opt):
+        assert "### sense difficulty" in opt
+
+    def test1(_, opt):
+        assert "Provide a number between `1` (very easy)" in opt
+
+    def test2(_, opt):
+        assert "Use these tasks as your **anchor point**" in opt
+
+    def test3(_, opt):
+        assert "- `3` Correct a single typo or awkward word " in opt
+
+    def test4(_, opt):
+        assert "- `50` Fix a misunderstanding caused by missing" in opt
+
+    def test5(_, opt):
+        assert "- `75` Choose and apply an appropriate common" in opt
+
+    def test6(_, opt):
+        assert "- `96` Integrate a standard external source" in opt
+
+    def test7(_, opt):
+        assert "- `100` Refactor a messy, ambiguous" in opt
+
 
 # TODO
--- tests/api/ky/sense/conftest.py
+++ tests/api/ky/sense/conftest.py
@@ -6,8 +6,3 @@ import pytest
 @pytest.fixture(scope="session")
 def sense_endpoint(app_endpoint):
     return app_endpoint + "/sense"
-
-
-@pytest.fixture
-def request_body():
-    return {"pre_sense_role": "", "difficulty_override": 0}
