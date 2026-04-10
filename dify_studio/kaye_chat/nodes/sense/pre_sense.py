96deff83f2ac7fa07b90087885360e428b4a3827
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-10 01:33:22 -0700

    add json import, enhance main() with json.dumps for request body
    ~`pre_sense.py`

--- dify_studio/kaye_chat/nodes/sense/pre_sense.py
+++ dify_studio/kaye_chat/nodes/sense/pre_sense.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-module-docstring
 
+import json
 
 # Output Keys  #################################################################
 
@@ -7,9 +8,19 @@ OUTPUT_BODY_KEY = "getter_request_body"
 OUTPUT_QUERY_KEY = "filtered_query"
 
 
+# Body Key  ####################################################################
+BODY_ROLE_KEY = "pre_sense_role"
+BODY_DIFF_KEY = "difficulty_override"
+
+
 # Entry Point  #################################################################
 def main(current_role: str, difficulty_override: float, query: str):
-    getter_body = ""
+    # body  --------------------------------------------------------------------
+    getter_body = json.dumps(
+        {BODY_ROLE_KEY: current_role, BODY_DIFF_KEY: difficulty_override}
+    )
+
+    # filter query  ------------------------------------------------------------
 
     filtered_query = ""
     # TODO ky: smart sense, take only part of the user's input as sense message:
