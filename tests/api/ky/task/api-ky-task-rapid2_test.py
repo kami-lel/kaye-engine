4a885f21af42fcf84e164b0b142317c3f16001e8
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-10 12:42:17 -0700

    Remove outdated comment in test setup
    ~`api-ky-task-rapid2_test.py`

--- tests/api/ky/task/api-ky-task-rapid2_test.py
+++ tests/api/ky/task/api-ky-task-rapid2_test.py
@@ -20,7 +20,6 @@ from tests.api.ky.task import *
 
 @pytest.fixture(scope="class")
 def opt(flask_test_client, task_endpoint):
-    # TODO need update
     payload = json.dumps({"role": "rapid", "programming_languages": "abc"})
 
     response = flask_test_client.post(
