ba13e9cd3d6251c94be1abdc6f7803471a049c8e
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-10 01:31:23 -0700

    Refactor pre_sense.py: add constants, streamline main function, placeholder for smart sense logic
    ~`pre_sense.py`

--- dify_studio/kaye_chat/nodes/sense/pre_sense.py
+++ dify_studio/kaye_chat/nodes/sense/pre_sense.py
@@ -1,3 +1,19 @@
-# TODO ky: smart sense, take only part of the user's input as sense message:
-# eg by taking first n lines + last n lines
-# eg by removing all codes block out
+# pylint: disable=missing-module-docstring
+
+
+# Output Keys  #################################################################
+
+OUTPUT_BODY_KEY = "getter_request_body"
+OUTPUT_QUERY_KEY = "filtered_query"
+
+
+# Entry Point  #################################################################
+def main(current_role: str, difficulty_override: float, query: str):
+    getter_body = ""
+
+    filtered_query = ""
+    # TODO ky: smart sense, take only part of the user's input as sense message:
+    # eg by taking first n lines + last n lines
+    # eg by removing all codes block out
+
+    return {OUTPUT_BODY_KEY: getter_body, OUTPUT_QUERY_KEY: filtered_query}
