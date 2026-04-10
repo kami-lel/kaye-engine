1ac99ca8eed89c1a25e0c56a22134e2d62c967c0
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-10 01:43:47 -0700

    migrate `body` tests from `post_start` to `pre_sense`; inline `_assert_structure`
    ^`dify-ky-pre_sense_test.py` Add unit tests for pre_sense node in Kaye Chat app
    ~`dify-ky-post_start_test.py` Refactor test structure, remove redundant body tests

--- tests/dify/ky/dify-ky-pre_sense_test.py
+++ tests/dify/ky/dify-ky-pre_sense_test.py
@@ -6,7 +6,14 @@ Unit Tests (using pytest) for:
 ``pre_sense`` node of Kaye Chat Dify App
 """
 
+import json
+
+
 import pytest
+
+
+from dify_studio.kaye_chat.nodes.sense import pre_sense
+from dify_studio.kaye_chat.nodes.sense.pre_sense import OUTPUT_BODY_KEY
 
 # helpers  #####################################################################
 
@@ -21,3 +28,37 @@ def kwargs():
 
 
 # TODO unit test for pre_sense
+
+
+class TestBody:
+
+    def test1(_, kwargs):
+
+        opt = pre_sense.main(**kwargs)
+        print(opt)
+
+        body = json.loads(opt[OUTPUT_SENSE_BODY_KEY])
+        print(body)
+
+        assert body == {"pre_sense_role": "", "difficulty_override": 0}
+
+    def test2(_, kwargs):
+
+        opt = pre_sense.main(**kwargs)
+        print(opt)
+
+        body = json.loads(opt[OUTPUT_SENSE_BODY_KEY])
+        print(body)
+
+        assert body == {"pre_sense_role": "", "difficulty_override": 50}
+
+    def test4(_, opt_coder_dft):
+
+        opt = pre_sense.main(**kwargs)
+        print(opt)
+
+        opt = opt_coder_dft
+        body = json.loads(opt[OUTPUT_SENSE_BODY_KEY])
+        print(body)
+
+        assert body == {"pre_sense_role": "coder", "difficulty_override": 0}
