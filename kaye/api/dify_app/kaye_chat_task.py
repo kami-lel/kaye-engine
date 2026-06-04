9ac44f2bf9a11a2f2afc3a53e92783e32afbf5c1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-06-04 15:28:12 +0800

    split chat/coder blueprint, extract date-time and units prompts
    ^`kaye_chat_task.py` Add multiple blueprints including chat, coder, and specialized roles
    ~`create_blueprint.py` Refactors create_blueprint.py: replaces multiple blueprints with specific ones, removes unused code

--- kaye/api/dify_app/kaye_chat_task.py
+++ kaye/api/dify_app/kaye_chat_task.py
@@ -11,7 +11,8 @@ from flask import request, abort, Response
 from kaye.prompt import (
     create_rapid_blueprint,
     create_chat_blueprint,
-    create_coder_blueprint,
+    create_date_time_blueprint,
+    create_number_unit_blueprint,
 )
 
 # constant  ####################################################################
@@ -39,10 +40,10 @@ def kaye_chat_task():
         bp = _create_changelog_blueprint()
 
     elif role == "chat":
-        bp = create_chat_blueprint()
+        bp = _create_chat_blueprint()
 
     elif role == "coder":
-        bp = create_coder_blueprint(plcs)
+        bp = _create_coder_blueprint(plcs)
 
     elif role == "deutschlehrer":
         bp = _create_deutschlehrer_blueprint()
@@ -82,64 +83,132 @@ def kaye_chat_task():
 # task blueprints  #############################################################
 
 
-def _create_art_blueprint():  # ================================================
+def _create_chat_blueprint():
+    bp = (
+        create_chat_blueprint()
+        | create_date_time_blueprint()
+        | create_number_unit_blueprint()
+    )
+    return bp
+
+
+def _create_coder_blueprint(plcs):
+    # create base bp from chat
+    bp = _create_chat_blueprint()
+
+    # add styles
+    bp.checkmark("Style", recursively=True)
+
+    # add ams
+    bp.checkmark(bp.corpus["Elements"]["Annotation Markers"], recursively=True)
+
+    # add Kaye Peer Coder node
+    kyc_node = bp.corpus["Kaye Peer Coder"]
+    bp.checkmark(kyc_node)
+
+    # adds PL nodes  -----------------------------------------------------------
+    for plc in plcs.split(","):
+        if plc == "bash":
+            bp.checkmark(kyc_node["Bash"])
+
+        elif plc == "c":
+            bp.checkmark(kyc_node["C"])
+            bp.checkmark(kyc_node["Brace Style"])
+
+        elif plc == "cpp":
+            bp.checkmark(kyc_node["C"])
+            bp.checkmark(kyc_node["C++"])
+            bp.checkmark(kyc_node["Brace Style"])
+
+        elif plc == "ue":
+            bp.checkmark(kyc_node["C"])
+            bp.checkmark(kyc_node["C++"])
+            bp.checkmark(kyc_node["Unreal Engine"])
+            bp.checkmark(kyc_node["Brace Style"])
+
+        elif plc == "csharp":
+            bp.checkmark(kyc_node["C Sharp"])
+            bp.checkmark(kyc_node["Brace Style"])
+
+        elif plc == "u3d":
+            bp.checkmark(kyc_node["C Sharp"])
+            bp.checkmark(kyc_node["Unity Engine"], recursively=True)
+            bp.checkmark(kyc_node["Brace Style"])
+
+        elif plc == "gdscript":
+            bp.checkmark(kyc_node["GDScript"])
+
+        elif plc == "html":
+            bp.checkmark(kyc_node["HTML"])
+
+        elif plc in ("js", "ts"):
+            bp.checkmark(kyc_node["JavaScript & TypeScript"], recursively=True)
+            bp.checkmark(kyc_node["Brace Style"])
+
+        elif plc == "py":
+            bp.checkmark(kyc_node["Python"], recursively=True)
+
+    return bp
+
+
+def _create_art_blueprint():
     bp = create_rapid_blueprint()
     bp.checkmark("Art Tutor", recursively=True)
     return bp
 
 
-def _create_barista_blueprint():  # ============================================
+def _create_barista_blueprint():
     bp = create_rapid_blueprint()
     bp.checkmark("Date & Time Format")
     bp.checkmark("Assistant Barista", recursively=True)
     return bp
 
 
-def _create_changelog_blueprint():  # ==========================================
-    bp = create_chat_blueprint()
+def _create_changelog_blueprint():
+    bp = _create_chat_blueprint()
     bp.checkmark("Changelog Writer", recursively=True)
     return bp
 
 
-def _create_deutschlehrer_blueprint():  # ======================================
-    bp = create_chat_blueprint()
+def _create_deutschlehrer_blueprint():
+    bp = _create_chat_blueprint()
     bp.checkmark("Deutschlehrer")
     return bp
 
 
-def _create_editor_blueprint():  # =============================================
-    bp = create_chat_blueprint()
+def _create_editor_blueprint():
+    bp = _create_chat_blueprint()
     bp.checkmark(bp.corpus["Style"]["Good Writing"])
     bp.checkmark(bp.corpus["Role"]["Editor"], recursively=True)
     return bp
 
 
-def _create_librarian_blueprint():  # ==========================================
-    bp = create_chat_blueprint()
+def _create_librarian_blueprint():
+    bp = _create_chat_blueprint()
     bp.checkmark("Librarian", recursively=True)
     return bp
 
 
-def _create_prompt_blueprint():  # =============================================
+def _create_prompt_blueprint():
     bp = create_rapid_blueprint()
     bp.checkmark("Prompt Writer")
     return bp
 
 
-def _create_secretary_blueprint():  # ==========================================
-    bp = create_chat_blueprint()
+def _create_secretary_blueprint():
+    bp = _create_chat_blueprint()
     bp.checkmark(bp.corpus["Style"]["Good Writing"])
     bp.checkmark(bp.corpus["Role"]["Secretary"])
     return bp
 
 
-def _create_shelver_blueprint():  # ============================================
-    bp = create_chat_blueprint()
+def _create_shelver_blueprint():
+    bp = _create_chat_blueprint()
     bp.checkmark(bp.corpus["Role"]["Shelver"], recursively=True)
     return bp
 
 
-def _create_tarot_blueprint():  # ==============================================
+def _create_tarot_blueprint():
     bp = create_rapid_blueprint()
     bp.checkmark("Tarot Reader", recursively=True)
     return bp
--- kaye/prompt/create_blueprint.py
+++ kaye/prompt/create_blueprint.py
@@ -9,7 +9,8 @@ from kaye.prompt.prompt_blueprint import PromptBlueprint
 __all__ = (
     "create_rapid_blueprint",
     "create_chat_blueprint",
-    "create_coder_blueprint",
+    "create_date_time_blueprint",
+    "create_number_unit_blueprint",
 )
 
 
@@ -29,9 +30,6 @@ CHAT_PROMPT_BLUEPRINT = """    ○
 [x] ├── Introduction
 [x] ├── Personality
 [x] ├── Language
-[x] ├── Elements
-[x] │   ├── Date & Time Format
-[x] │   └── Numerical Values with Units
 [x] ├── Format
 [x] ├── Role
 [x] └── {Abbreviations}"""
@@ -48,60 +46,13 @@ def create_chat_blueprint():
     return PromptBlueprint.parse(CHAT_PROMPT_BLUEPRINT)
 
 
-def create_coder_blueprint(plcs):  # TODO split various blueprints
-    # create base bp from chat
-    bp = create_chat_blueprint()
+def create_date_time_blueprint():
+    return PromptBlueprint.parse("""    ○
+[x] └── Elements
+[x]     └── Date & Time Format""")
 
-    # add styles
-    bp.checkmark("Style", recursively=True)
 
-    # add ams
-    bp.checkmark(bp.corpus["Elements"]["Annotation Markers"], recursively=True)
-
-    # add Kaye Peer Coder node
-    kyc_node = bp.corpus["Kaye Peer Coder"]
-    bp.checkmark(kyc_node)
-
-    # adds PL nodes  -----------------------------------------------------------
-    for plc in plcs.split(","):
-        if plc == "bash":
-            bp.checkmark(kyc_node["Bash"])
-
-        elif plc == "c":
-            bp.checkmark(kyc_node["C"])
-            bp.checkmark(kyc_node["Brace Style"])
-
-        elif plc == "cpp":
-            bp.checkmark(kyc_node["C"])
-            bp.checkmark(kyc_node["C++"])
-            bp.checkmark(kyc_node["Brace Style"])
-
-        elif plc == "ue":
-            bp.checkmark(kyc_node["C"])
-            bp.checkmark(kyc_node["C++"])
-            bp.checkmark(kyc_node["Unreal Engine"])
-            bp.checkmark(kyc_node["Brace Style"])
-
-        elif plc == "csharp":
-            bp.checkmark(kyc_node["C Sharp"])
-            bp.checkmark(kyc_node["Brace Style"])
-
-        elif plc == "u3d":
-            bp.checkmark(kyc_node["C Sharp"])
-            bp.checkmark(kyc_node["Unity Engine"], recursively=True)
-            bp.checkmark(kyc_node["Brace Style"])
-
-        elif plc == "gdscript":
-            bp.checkmark(kyc_node["GDScript"])
-
-        elif plc == "html":
-            bp.checkmark(kyc_node["HTML"])
-
-        elif plc in ("js", "ts"):
-            bp.checkmark(kyc_node["JavaScript & TypeScript"], recursively=True)
-            bp.checkmark(kyc_node["Brace Style"])
-
-        elif plc == "py":
-            bp.checkmark(kyc_node["Python"], recursively=True)
-
-    return bp
+def create_number_unit_blueprint():
+    return PromptBlueprint.parse("""    ○
+[x] └── Elements
+[x]     └── Numerical Values with Units""")
