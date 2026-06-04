0d62ad1ecfdad8fa822a0c16a99c985659e25622
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-06-04 18:30:27 +0800

    Add todo note for writer of readme, changelog, docs in embedded_blueprints.py
    ?`embedded_blueprints.py`

--- kaye/prompt/embedded_blueprints.py
+++ kaye/prompt/embedded_blueprints.py
@@ -95,7 +95,7 @@ continue_behavior_blueprint.checkmark(
 )
 continue_behavior_blueprint.display_name = "Continue Behavior"
 
-# todo changelog writer
+# todo writer for: readme,changelog,docs
 
 # coder  =======================================================================
 
