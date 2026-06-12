bfe5b8b74c9d795e0cc4f08ead2df9f8115a5931
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-06-12 17:09:13 +0800

    Add placeholder for display name to skill name conversion function
    ~`export_skill.py`

--- kaye/cli/cli_skill/export_skill.py
+++ kaye/cli/cli_skill/export_skill.py
@@ -5,5 +5,12 @@ define ``export_skills_as_folders``
 """
 
 
+def _convert_display_name2skill_name(display_name):
+    pass
+
+
 def export_skills_as_folders(parent_folder):
-    pass  # TODO
+    # TODO export embedded blueprints
+    # TODO export prompts
+    # TODO export abbrs
+    pass
