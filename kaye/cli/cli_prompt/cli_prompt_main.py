87db1d58f26e8ca389a77883d1ab1d5f9410bff1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-06-14 01:36:52 +0800

    **group cli_prompt modules under subpackage**
    =`cli_prompt_generate.py` Rename file for better directory structure organization
    =`cli_prompt_ls.py` Rename file to reflect directory structure change
    =`cli_prompt_main.py` rename module to cli_prompt with minor updates
    =`cli_prompt_show.py` Rename file to match directory structure

--- kaye/cli/cli_prompt/cli_prompt_main.py
+++ kaye/cli/cli_prompt/cli_prompt_main.py
@@ -3,9 +3,11 @@ as a subset of the prompt corpus"""
 
 # fixme standardize module docstring & help & description
 
-from kaye.cli.cli_prompt_ls import register_cli_prompt_ls_parser
-from kaye.cli.cli_prompt_show import register_cli_prompt_show_parser
-from kaye.cli.cli_prompt_generate import register_cli_prompt_generate_parser
+from kaye.cli.cli_prompt.cli_prompt_ls import register_cli_prompt_ls_parser
+from kaye.cli.cli_prompt.cli_prompt_show import register_cli_prompt_show_parser
+from kaye.cli.cli_prompt.cli_prompt_generate import (
+    register_cli_prompt_generate_parser,
+)
 
 
 def register_cli_prompt_parser(cli_subparser):
