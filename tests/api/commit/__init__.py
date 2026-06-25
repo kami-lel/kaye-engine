ab4f37c322f84355b545de8023876cf8c6909bb2
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-06-26 02:13:08 +0800

    Remove placeholder comments, improve test pattern, suppress prints
    ~`__init__.py`

--- tests/api/commit/__init__.py
+++ tests/api/commit/__init__.py
@@ -1,5 +1,8 @@
 from tests.api import assert_briefness_style
 
+# FIXME use new pattern testing
+# HACK remove all print out in unit tests
+
 
 def assert_allows_md(opt):
     assert """## no markdown syntax
