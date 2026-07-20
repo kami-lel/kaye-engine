ac52a7ae682ab89016dd4727fb0dbf7a3772cd7d
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-20 23:14:10 +0800

    fix module docstring; remove inline BUG/FIXME; simplify help callback
    #`cli_prompt_main.py`

--- kaye/cli/prompt/main_parser.py
+++ kaye/cli/prompt/main_parser.py
@@ -8,26 +8,33 @@ from kaye.cli.prompt.ls_parser import register_ls_subparser
 from kaye.cli.prompt.show_parser import register_show_subparser
 from kaye.cli.prompt.generate_parser import register_generate_subparser
 
+# FIXME upd main doc
+
 _HELP = (
     "dynamically generate system prompt with a prompt blueprint "
     "as a subset of the prompt corpus"
 )
 
 
+_DESCRIPTION = _HELP + """
+
+more description"""
+
+
 def register_cli_prompt_parser(cli_subparser):  ################################
-    """register the ``kaye prompt`` subcommand parser"""
+    """
+    register the ``kaye prompt`` subcommand parser
+    """
     cli_prompt_parser = cli_subparser.add_parser(
         "prompt",
         help=_HELP,
-        description=_HELP,
+        description=_DESCRIPTION,
         aliases=["p"],
     )
 
-    def _cli_prompt_parser_main(_):
-        # when calling ``python -m kaye prompt``
-        cli_prompt_parser.print_help()
-
-    cli_prompt_parser.set_defaults(func=_cli_prompt_parser_main)
+    cli_prompt_parser.set_defaults(
+        func=lambda _: cli_prompt_parser.print_help()
+    )
 
     cli_prompt_subparser = cli_prompt_parser.add_subparsers(
         description="utility functions related to prompt generation"
