03925a726e57c8a40ba380d0112058c5598621c9
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-22 20:41:13 +0800

    add merge_final_answer node scaffold with TODO merge logic
    ^`merge_final_answer.py`

---
+++ dify_studio/kaye_commit_sense/nodes/merge_final_answer.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-module-docstring
+
+
+# output keys  #################################################################
+OUTPUT_ANSWER = "answer"
+
+
+# constants  ###################################################################
+
+
+# Entry Point  #################################################################
+def main(
+    allows_md: bool,
+    filenames: list[str],
+    per_file_extracts: list[dict],
+    primary_message: str,
+):
+    # TODO TODO write the merging logic
+    answer = ""
+    return {OUTPUT_ANSWER: answer}
