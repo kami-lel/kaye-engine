808be898c6e883bf4d2f702e8211293ca2699e21
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-30 02:25:15 +0800

    remove prompt corpus

--- kaye_engine/__init__.py
+++ kaye_engine/__init__.py
@@ -2,9 +2,14 @@
 Kaye Python Package API
 """
 
-
-
 from kaye_engine import kamilog
+
+__all__ = (
+    "DISPLAY_NAME",
+    "PROGRAM_NAME",
+    "get_corpus_tree",
+    "load_corpus_tree",
+)
 
 
 PROGRAM_NAME = "kaye-engine"
@@ -12,3 +17,6 @@ DISPLAY_NAME = "Prompt Engineering Project Kaye Engine"
 
 
 logger = kamilog.getLogger(PROGRAM_NAME)
+
+
+from kaye_engine.prompt import load_corpus_tree, get_corpus_tree  # noqa: I001
