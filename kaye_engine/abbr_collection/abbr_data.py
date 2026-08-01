91db679d8956d9429a0690837ff24a5db4142835
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-08-02 01:45:28 +0800

    fixme abbr data need to warn for empty abbrs
     @`abbr_data.py`

--- kaye_engine/abbr_collection/abbr_data.py
+++ kaye_engine/abbr_collection/abbr_data.py
@@ -113,4 +113,5 @@ def get_abbr_data():  # ========================================================
             if nothing has been added yet
     :rtype: AbbrData
     """
+    # fixme abbr data need to warn for empty abbrs
     return _abbr_data
