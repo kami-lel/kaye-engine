225dace8801aa8c65108319a96f60ac16c628820
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-08-02 21:57:10 +0800

    raise Fixme to FIXME for cli prompt file-flag behavior
    @`__init__.py`

--- kaye_engine/cli/prompt/__init__.py
+++ kaye_engine/cli/prompt/__init__.py
@@ -5,4 +5,4 @@ mark ``kaye_engine.cli.prompt`` as the package for prompt blueprint listing
 and generation subcommands
 """
 
-# Fixme cli prompt, -F for file is not bash-like
+# FIXME cli prompt, -F for file is not bash-like
