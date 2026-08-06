3600a16b90d09e82313ff63514de06268b16ca05
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-08-06 18:22:35 +0800

    introduce abbr group registry; export register/get abbr group
    adds `AbbrGroupRegistry` singleton with public register/get api,
    wired through `abbr_collection` and top-level package exports
    
    📄`__init__.py` expose abbr group registration and retrieval functions in public API
    📄`__init__.py` expose abbr group registry functions and registry instance

--- kaye_engine/abbr_collection/__init__.py
+++ kaye_engine/abbr_collection/__init__.py
@@ -7,6 +7,12 @@ define various data structures supporting **abbreviation nodes**
 from .abbr_tags import AbbrTags
 from .abbr_wrap import AbbrWrap
 from .abbr_group import ABBRS_JSON_GROUP_KEY, AbbrGroupIndex
+from .abbr_group_registry import (
+    AbbrGroupRegistry,
+    abbr_group_registry,
+    register_abbr_group,
+    get_abbr_group,
+)
 from .abbr_data import AbbrData, get_abbr_data
 from .abbr_data_loader import populate_abbr_data_with_json_file
 from .abbr_meaning import AbbrMeaning
@@ -17,6 +23,10 @@ __all__ = (
     "AbbrWrap",
     "ABBRS_JSON_GROUP_KEY",
     "AbbrGroupIndex",
+    "AbbrGroupRegistry",
+    "abbr_group_registry",
+    "register_abbr_group",
+    "get_abbr_group",
     "AbbrData",
     "get_abbr_data",
     "populate_abbr_data_with_json_file",
